d184952409ca5a7ce39dd270620a4b1170b2fe52
yellowtoast <kjh9519@naver.com>
2022-04-19 23:16:42 +0900

    color and theme set

---
+++ withconi/lib/ui/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color blue100 = Color(0xff0058DD);
+
+const Color grey80 = Color(0xffF0F1F2);
+const Color grey100 = Color(0xffD2D2D2);
+const Color grey120 = Color(0xff979797);
+
+const Color black = Color(0xff161616);
+
+const Color white = Colors.white;
---
+++ withconi/lib/ui/theme/sizes.dart
@@ -0,0 +1,4 @@
+import 'package:get/get.dart';
+
+final double WCwidth = Get.width;
+final double WCheight = Get.height;
