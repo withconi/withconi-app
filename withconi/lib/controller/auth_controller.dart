526d51575cd2ff3dac794036c13d37c6e1fc4572
yellowtoast <kjh9519@naver.com>
2022-04-20 17:17:18 +0900

    added auth controller

---
+++ withconi/lib/controller/auth_controller.dart
@@ -0,0 +1,85 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import '../configs/sns_login/firebase_auth_constants.dart';
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+  late Rx<User?> firebaseUser;
+  late Rx<GoogleSignInAccount?> googleSignInAccount;
+
+  @override
+  void onReady() {
+    super.onReady();
+
+    firebaseUser = Rx<User?>(auth.currentUser);
+    googleSignInAccount = Rx<GoogleSignInAccount?>(googleSign.currentUser);
+
+    firebaseUser.bindStream(auth.userChanges());
+    ever(firebaseUser, _setInitialScreen);
+
+    googleSignInAccount.bindStream(googleSign.onCurrentUserChanged);
+    ever(googleSignInAccount, _setInitialScreenGoogle);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user == null) {
+      // Get.offAll(() => const Register());
+    } else {
+      // Get.offAll(() => Home());
+    }
+  }
+
+  _setInitialScreenGoogle(GoogleSignInAccount? googleSignInAccount) {
+    print(googleSignInAccount);
+    if (googleSignInAccount == null) {
+      // Get.offAll(() => const Register());
+    } else {
+      // Get.offAll(() => Home());
+    }
+  }
+
+  void signInWithGoogle() async {
+    try {
+      GoogleSignInAccount? googleSignInAccount = await googleSign.signIn();
+
+      if (googleSignInAccount != null) {
+        GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+
+        AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+
+        await auth
+            .signInWithCredential(credential)
+            .catchError((onErr) => print(onErr));
+      }
+    } catch (e) {
+      Get.snackbar(
+        "Error",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+      print(e.toString());
+    }
+  }
+
+  void register(String email, password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } catch (firebaseAuthException) {}
+  }
+
+  void login(String email, password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (firebaseAuthException) {}
+  }
+
+  void signOut() async {
+    await auth.signOut();
+  }
+}
