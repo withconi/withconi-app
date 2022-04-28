b477aae6faaf02cc5fbf6a9c3356842b84f6f052
yellowtoast <kjh9519@naver.com>
2022-04-28 20:50:06 +0900

    constants modified into a class format

---
+++ withconi/lib/configs/constants/constants.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+class Constants {
+  static const WITHCONI_NATIVE_APP_KEY = '2bd59238f56f29a17c2bdb0fb628c83d';
+  static const WITHCONI_DB_API_KEY = "WC_API_KEY";
+}
