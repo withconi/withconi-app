066180b6eadac99a11d5c26e81c5eb729f809c7d
yellowtoast <kjh9519@naver.com>
2022-05-11 15:27:09 +0900

    auth repository edited

--- withconi/lib/data/repository/auth_repository.dart
+++ withconi/lib/data/repository/auth_repository.dart
@@ -1,11 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:withconi/configs/constants/enum.dart';
+import 'package:withconi/configs/helpers/token_manager.dart';
 import 'package:withconi/data/model/token.dart';
 import 'package:withconi/data/provider/auth_api.dart';
 
 class AuthRepository {
   final AuthAPI _api = Get.find<AuthAPI>();
+  final TokenManager _tokenManager = Get.find<TokenManager>();
 
-  Future<TokenModel> newToken() => _api.newToken();
+  // Future<TokenModel> newToken() => _api.newCustomAuthToken();
+
+  ProviderOptions getAuthTokenProvider() => _tokenManager.getTokenProvider();
+
+  Future<UserCredential?> signUpWithEmail() => _api.signUpWithEmail();
+
+  Future<UserCredential?> signInWithEmail() => _api.signInWithEmail();
+
+  Future<UserCredential?> signInWithGoogle() => _api.signInWithGoogle();
+
+  Future<UserCredential?> signInWithToken() => _api.signInWithToken();
+
+  bool isUserLoggedIn({required ProviderOptions provider}) =>
+      _api.isUserLoggedIn(provider: provider);
 
   Future<TokenModel> authWithLogin({
     required String email,
