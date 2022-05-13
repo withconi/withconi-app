5916e0a2cff9043c7715fafbad1b21b40aa458c9
yellowtoast <kjh9519@naver.com>
2022-05-13 20:49:40 +0900

    authController edited

--- withconi/lib/controller/auth_controller.dart
+++ withconi/lib/controller/auth_controller.dart
@@ -1,33 +1,38 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:withconi/configs/constants/enum.dart';
-import 'package:withconi/configs/constants/firebase_auth_constants.dart';
+import 'package:withconi/configs/constants/auth_variables.dart';
 import 'package:withconi/data/repository/auth_repository.dart';
 import 'package:withconi/ui/pages/start/start_page.dart';
 import '../import_basic.dart';
 
 class AuthController extends GetxController {
   static AuthController instance = Get.find();
-  final AuthRepository _authRepository = Get.find<AuthRepository>();
-  late Rx<ProviderOptions> _provider;
-  final FirebaseAuth _auth = auth;
+  final AuthRepository _authRepository = AuthRepository();
+  final Rx<ProviderOptions> provider = ProviderOptions.NONE.obs;
   final RxBool _isLoggedIn = false.obs;
 
   @override
-  void onReady() {
-    super.onReady();
-    _provider.value = _authRepository.getAuthTokenProvider();
-    _isLoggedIn.value =
-        _authRepository.isUserLoggedIn(provider: _provider.value);
-
-    once(_isLoggedIn, _setInitialPage);
+  onInit() {
+    super.onInit();
+    provider.value = _authRepository.getAuthTokenProvider();
   }
 
-  _setInitialPage(_isLoggedIn) async {
+  @override
+  onReady() async {
+    super.onReady();
+    _isLoggedIn.value =
+        await _authRepository.isUserLoggedIn(provider: provider.value);
+    await Future.delayed(const Duration(milliseconds: 1300), () {
+      _setInitialPage(_isLoggedIn.value);
+    });
+  }
+
+  _setInitialPage(bool _isLoggedIn) {
     if (_isLoggedIn) {
-      Get.offAll(() => const StartPage());
+      // Get.offAll(() => const HomePage());
     } else {
-      //  Get.offAll(() => const HomePage());
+      Get.offAllNamed(Routes.START);
     }
   }
 }
