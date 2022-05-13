f7cb558a2f060b3a650aa66747ac086ad31e02b0
yellowtoast <kjh9519@naver.com>
2022-05-13 20:51:25 +0900

    cache_manager edit

---
+++ withconi/lib/configs/helpers/cache_manager.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: constant_identifier_names
+import 'package:get_storage/get_storage.dart';
+import '../constants/enum.dart';
+
+mixin CacheManager {
+  Future<bool> saveCache(
+    CacheControllerKey cacheControllerKey,
+    String? value,
+  ) async {
+    final box = GetStorage();
+    await box.write(cacheControllerKey.toString(), value);
+    return true;
+  }
+
+  String getCache(CacheControllerKey cacheControllerKey) {
+    final box = GetStorage();
+    String? cacheResult = box.read(cacheControllerKey.toString());
+    return (cacheResult == null) ? '' : cacheResult;
+  }
+
+  Future<void> removeCache(CacheControllerKey cacheControllerKey) async {
+    final box = GetStorage();
+    await box.remove(cacheControllerKey.toString());
+  }
+}
