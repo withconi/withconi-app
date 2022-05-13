fc70864045672fa7673190302ab11f372ee7af77
yellowtoast <kjh9519@naver.com>
2022-05-13 20:51:13 +0900

    enum edited

---
+++ withconi/lib/configs/constants/enum.dart
@@ -0,0 +1,4 @@
+// ignore_for_file: constant_identifier_names
+
+enum ProviderOptions { KAKAO, NAVER, GOOGLE, APPLE, EMAIL, NONE }
+enum CacheControllerKey { ACCESS_TOKEN, REFRESH_TOKEN, TOKEN_PROVIDER }
