1c99a9d46af3796bbf730afb74fc6176467ca74e
yellowtoast <kjh9519@naver.com>
2022-06-05 13:27:26 +0900

    fit: implemented start_controller

--- withconi/lib/controller/start_controller.dart
+++ withconi/lib/controller/start_controller.dart
@@ -1,34 +1,75 @@
-import 'package:withconi/data/model/token.dart';
+import 'package:withconi/configs/constants/auth_variables.dart';
+import 'package:withconi/configs/constants/enum.dart';
+import 'package:withconi/controller/signup/user_data.dart';
 import 'package:withconi/data/repository/auth_repository.dart';
-
 import '../import_basic.dart';
 
 class StartPageController extends GetxController {
-  final _repository = Get.find<AuthRepository>();
+  final AuthRepository _repository = AuthRepository();
+  late RxString _email;
+  late RxBool validateButton;
 
-  String _email = '', _password = '';
+  String get email => _email.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _email = ''.obs;
+    validateButton = false.obs;
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    debounce(_email, validateNextButton, time: 1.seconds);
+  }
 
   void onEmailChange(String email) {
-    _email = email;
+    _email.value = email;
   }
 
-  void onPasswordChange(String password) {
-    _password = password;
+  validateNextButton(_email) async {
+    validateButton.value = await _repository.checkDuplicateEmail(email: _email);
+    print('validate Button 결과 : ${validateButton.value}');
   }
 
-  Future<void> onSignInPressed() async {
-    try {
-      final requestToken = Get.arguments as String;
-
-      /// call data arguments to from Get.offNamed (event push data Controller)
-      print('[REQUEST TOKEN] =======> [ $requestToken ]');
-      final TokenModel authToken = await _repository.authWithLogin(
-        email: _email,
-        password: _password,
-        token: requestToken,
-      );
-    } catch (e) {
-      print(e);
+  signIn(ProviderOptions provider) {
+    switch (provider) {
+      case ProviderOptions.EMAIL:
+        // _repository.signInWithEmail(
+        //     email: _email.value, password: _password.value);
+        break;
+      case ProviderOptions.GOOGLE:
+        _repository.signInWithGoogle();
+        break;
+      case ProviderOptions.KAKAO:
+        _repository.signInWithToken(provider: authController.provider.value);
+        break;
+      case ProviderOptions.NAVER:
+        _repository.signInWithToken(provider: authController.provider.value);
+        break;
+      default:
     }
   }
+
+  nextStep() {
+    UserData.to.onEmailChange(email);
+    Get.toNamed(Routes.SIGNUP_NAME_PW);
+  }
+
+  // Future<void> onSignInPressed() async {
+  //   try {
+  //     final requestToken = Get.arguments as String;
+
+  //     /// call data arguments to from Get.offNamed (event push data Controller)
+  //     print('[REQUEST TOKEN] =======> [ $requestToken ]');
+  //     final TokenModel authToken = await _repository.authWithLogin(
+  //       email: _email,
+  //       password: _password,
+  //       token: requestToken,
+  //     );
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 }
