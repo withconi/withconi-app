2c6ff92b3ac1b7f586336be23d206ca8867992d3
yellowtoast <kjh9519@naver.com>
2022-06-06 15:34:04 +0900

    edit: edited locales and theme for main.dart

--- withconi/lib/main.dart
+++ withconi/lib/main.dart
@@ -1,25 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import 'package:withconi/routes/withconi_pages.dart';
-import 'configs/sns_login/firebase_auth_constants.dart';
-import 'configs/sns_login/kakao_constants.dart';
-import 'controller/auth_controller.dart';
-import 'routes/withconi_routes.dart';
+import 'package:withconi/configs/constants/auth_variables.dart';
+import 'import_basic.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  KakaoSdk.init(nativeAppKey: WITHCONI_NATIVE_APP_KEY);
-  await firebaseInitialization.then((value) {
-    Get.put(AuthController());
-  });
+  KakaoSdk.init(nativeAppKey: Constants.WITHCONI_NATIVE_APP_KEY);
+  await firebaseInitialization;
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: Routes.START,
-    // theme: appThemeData,
-    defaultTransition: Transition.fade,
+    theme: ThemeData(scaffoldBackgroundColor: WcColors.white),
+    initialRoute: Routes.SPLASH,
+    defaultTransition: Transition.cupertino,
     getPages: WCPages.pages,
-    // locale: Locale('pt', 'BR'),
+    localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+    ],
+    supportedLocales: [
+      const Locale('ko', 'KR'),
+    ],
+
     // translationsKeys: AppTranslation.translations,
   ));
 }
