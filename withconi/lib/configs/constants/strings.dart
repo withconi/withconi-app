0bd6bc6b5ee5f3f6f2dc138df953bc9d1c2b2c43
yellowtoast <kjh9519@naver.com>
2022-06-06 15:32:54 +0900

    edit: edited strings and colors files

--- withconi/lib/configs/constants/strings.dart
+++ withconi/lib/configs/constants/strings.dart
@@ -6,7 +6,7 @@ class Strings {
 class Validator {
   const Validator();
   String get email => '올바른 이메일 형식으로 입력해주세요';
-  String get password => '8자리 이상 영문과 숫자 혹은 기호 조합';
+  String get password => '8자리 이상 영문/숫자/기호';
   String get confirmPassword => '비밀번호가 다릅니다';
   String get name => '1~8자 이내';
 }
--- withconi/lib/ui/theme/colors.dart
+++ withconi/lib/ui/theme/colors.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-const Color blue100 = Color(0xff0058DD);
-
-const Color grey80 = Color(0xffF0F1F2);
-const Color grey100 = Color(0xffD2D2D2);
-const Color grey120 = Color(0xff979797);
-
-const Color black = Color(0xff161616);
-
-const Color white = Colors.white;
+class WcColors {
+  WcColors._();
+  static const Color blue100 = Color(0xff0058DD);
+  static const Color grey80 = Color.fromARGB(255, 237, 237, 237);
+  static const Color grey100 = Color.fromARGB(255, 205, 205, 205);
+  static const Color grey120 = Color(0xff979797);
+  static const Color black = Color(0xff161616);
+  static const Color grey200 = Color.fromARGB(255, 54, 54, 54);
+  static const Color white = Colors.white;
+}
