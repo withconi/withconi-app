41d810bc33aea679afe8f5efb527e3706ad62a40
yellowtoast <kjh9519@naver.com>
2022-06-06 15:36:20 +0900

    edit: edited DiseaseModel

--- withconi/lib/data/model/diseases.dart
+++ withconi/lib/data/model/diseases.dart
@@ -1,17 +1,23 @@
-class DiseaseModel {
+import 'package:equatable/equatable.dart';
+
+class DiseaseModel extends Equatable {
   String code;
   String name;
   List<String> symptoms;
   String description;
+  int createdAt;
 
-  DiseaseModel(
-      {required this.code,
-      required this.name,
-      required this.symptoms,
-      required this.description});
+  DiseaseModel({
+    required this.createdAt,
+    required this.code,
+    required this.name,
+    required this.symptoms,
+    required this.description,
+  });
 
   factory DiseaseModel.fromJson(Map<String, dynamic> json) {
     return DiseaseModel(
+        createdAt: json['createdAt'],
         code: json['code'],
         name: json['name'],
         symptoms: json['symptoms'],
@@ -19,9 +25,13 @@ class DiseaseModel {
   }
 
   Map<String, dynamic> toJson() => {
+        'createdAt': createdAt,
         'code': code,
         'name': name,
         'symptoms': symptoms,
         'description': description,
       };
+
+  @override
+  List<Object> get props => [this.code, this.name];
 }
