b39dc5c7a2022fcd8c109b32c2ed56002db32578
yellowtoast <kjh9519@naver.com>
2022-06-08 18:12:41 +0900

    edit: edited custom textfield

--- withconi/lib/ui/widgets/text_field/textfield.dart
+++ withconi/lib/ui/widgets/text_field/textfield.dart
@@ -9,38 +9,55 @@ class WcTextField extends StatelessWidget {
     required this.onChanged,
     required this.textController,
     required this.keyboardType,
-    this.obscureText = false,
+    this.textObscure = false,
+    this.enabled = true,
+    this.errorText,
+    this.labelText,
   }) : super(key: key);
 
   String hintText;
   void Function(String)? onChanged;
   TextEditingController textController;
+  String? labelText;
   TextInputType keyboardType;
-  bool obscureText;
+  bool textObscure;
+  bool enabled;
+  String? errorText;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      obscureText: obscureText,
+      enabled: enabled,
+      obscuringCharacter: '●',
+      obscureText: textObscure,
       controller: textController,
       keyboardType: keyboardType,
       style: GoogleFonts.notoSans(
-          color: WcColors.black, fontSize: 18, fontWeight: FontWeight.w500),
+          color: WcColors.black, fontSize: 18, fontWeight: FontWeight.w600),
       onChanged: onChanged,
       cursorColor: WcColors.blue100,
       decoration: InputDecoration(
+        isDense: true,
+        contentPadding: EdgeInsets.only(top: 10, bottom: 8),
+        errorText: errorText,
+        labelText: labelText,
+        labelStyle: GoogleFonts.notoSans(
+            color: WcColors.grey100, fontSize: 17, fontWeight: FontWeight.w300),
         floatingLabelStyle: GoogleFonts.notoSans(
             color: WcColors.black, fontSize: 17, fontWeight: FontWeight.w500),
         alignLabelWithHint: true,
         enabledBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: WcColors.grey100, width: 1),
+          borderSide: BorderSide(color: WcColors.grey100, width: 0.6),
+        ),
+        disabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: WcColors.grey100, width: 0.6),
         ),
         focusedBorder: const UnderlineInputBorder(
           borderSide: BorderSide(color: WcColors.blue100, width: 1.5),
         ),
         hintText: hintText,
         hintStyle: GoogleFonts.notoSans(
-            color: WcColors.grey100, fontSize: 17, fontWeight: FontWeight.w300),
+            color: WcColors.grey100, fontSize: 17, fontWeight: FontWeight.w400),
       ),
     );
   }
