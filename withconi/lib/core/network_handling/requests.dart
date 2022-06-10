30431bcefee7aab623ef98f52a0a100a705e350b
yellowtoast <kjh9519@naver.com>
2022-06-10 19:54:00 +0900

    remove: removed files related to network handling

--- withconi/lib/core/network_handling/requests.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-part 'requests.freezed.dart';
-
-@freezed
-class NetworkRequestBody with _$NetworkRequestBody {
-  const factory NetworkRequestBody.empty() = Empty;
-  const factory NetworkRequestBody.json(Map<String, dynamic> data) = Json;
-  const factory NetworkRequestBody.text(String data) = Text;
-}
