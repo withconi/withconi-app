ac615eb3acab4c2586018cc80d7e44a716e64fff
yellowtoast <kjh9519@naver.com>
2022-06-10 19:54:42 +0900

    add: added file for importing basic libraries

---
+++ withconi/lib/import_basic.dart
@@ -0,0 +1,8 @@
+export 'package:flutter/material.dart';
+export 'package:get/get.dart';
+export 'package:withconi/routes/withconi_pages.dart';
+export 'configs/constants/constants.dart';
+export 'routes/withconi_routes.dart';
+export 'package:withconi/ui/theme/colors.dart';
+export 'package:withconi/ui/theme/sizes.dart';
+export 'package:google_fonts/google_fonts.dart';
