5c9ca68a95c1490b8dc70d8095017ea97a9fa9ba
yellowtoast <kjh9519@naver.com>
2022-06-14 17:33:21 +0900

    add: added some features to auth_api

--- withconi/lib/data/provider/auth_api.dart
+++ withconi/lib/data/provider/auth_api.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:crypto/crypto.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:withconi/configs/constants/api_url.dart';
 import 'package:withconi/configs/constants/enum.dart';
 import 'package:withconi/configs/constants/auth_variables.dart';
@@ -52,8 +56,6 @@ class AuthAPI {
     }
     return null;
   }
-
-  signInWithApple() {}
 
   Future<AuthInfo> signInWithKakao() async {
     late String accessToken;
@@ -152,19 +154,22 @@ class AuthAPI {
   signUpUserDB() {}
 
   Future<bool> checkDuplicateEmail({required email}) async {
-    // final response = await _dio.post(
-    //   HttpUrl.VERIFY_EMAIL,
-    //   data: {
-    //     "email": email,
-    //   },
-    // );
-    // print('[API SUCCESS] ==== ${response.data.toString()}');
+    final response = await _dio.post(
+      HttpUrl.VERIFY_EMAIL,
+      data: {
+        "email": email,
+      },
+    );
+    print('[API SUCCESS] ==== ${response.data.toString()}');
 
-    if (email == 'kjh9519@naver.com') {
-      return true;
-    } else {
-      return false;
-    }
+    bool isDuplicateUser = response.data['data']['isAuth'];
+
+    return isDuplicateUser;
+    // if (email == 'kjh9519@naver.com') {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
   }
 
   Future<bool> isUserLoggedIn({required ProviderOptions provider}) async {
@@ -195,5 +200,55 @@ class AuthAPI {
       }
     }
     return _isLoggedIn;
+  }
+
+  /// Generates a cryptographically secure random nonce, to be included in a
+  /// credential request.
+  String generateNonce([int length = 32]) {
+    const charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  /// Returns the sha256 hash of [input] in hex notation.
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  Future<AuthInfo> signInWithApple() async {
+    // To prevent replay attacks with the credential returned from Apple, we
+    // include a nonce in the credential request. When signing in with
+    // Firebase, the nonce in the id token returned by Apple, is expected to
+    // match the sha256 hash of `rawNonce`.
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+    late final AuthInfo authInfo;
+
+    // Request credential for the currently signed in Apple account.
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: nonce,
+    );
+
+    // Create an `OAuthCredential` from the credential returned by Apple.
+    final oauthCredential = OAuthProvider("apple.com").credential(
+      idToken: appleCredential.identityToken,
+      rawNonce: rawNonce,
+    );
+    authInfo = AuthInfo(
+        authObject: oauthCredential,
+        email: appleCredential.email!,
+        provider: ProviderOptions.APPLE);
+
+    // Sign in the user with Firebase. If the nonce we generated earlier does
+    // not match the nonce in `appleCredential.identityToken`, sign in will fail.
+    return authInfo;
   }
 }
