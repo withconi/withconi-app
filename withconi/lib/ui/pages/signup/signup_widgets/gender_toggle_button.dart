490524e2613c43ce23aeada44b7b5ebd7fb2cec9
yellowtoast <kjh9519@naver.com>
2022-06-15 22:18:34 +0900

    fit: implemented genderToggleButton for UI

---
+++ withconi/lib/ui/pages/signup/signup_widgets/gender_toggle_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../import_basic.dart';
+
+class GenderToggleButton extends StatelessWidget {
+  GenderToggleButton({Key? key, this.onPressed, required this.selectionList})
+      : super(key: key);
+
+  Function(int)? onPressed;
+  List<bool> selectionList;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 43,
+      child: ToggleButtons(
+        onPressed: onPressed,
+        borderRadius: BorderRadius.circular(10),
+        children: [
+          // Icon(SvgPicture.asset('assets/icons/gender_female.svg')),
+          SvgPicture.asset('assets/icons/gender_female.svg',
+              height: 20,
+              color: (selectionList[0]) ? WcColors.red100 : WcColors.grey100),
+          SvgPicture.asset('assets/icons/gender_male.svg',
+              width: 18,
+              color: (selectionList[1]) ? WcColors.blue100 : WcColors.grey100),
+        ],
+        isSelected: selectionList,
+        selectedBorderColor:
+            (selectionList[0]) ? WcColors.red100 : WcColors.blue100,
+        selectedColor: (selectionList[0]) ? WcColors.blue100 : WcColors.black,
+        disabledColor: WcColors.grey120,
+        disabledBorderColor: WcColors.grey80,
+        fillColor: (selectionList[0]) ? WcColors.red20 : WcColors.blue40,
+        borderWidth: 1.2,
+      ),
+    );
+  }
+}
