e1637632a28b0ebc48b66957d762e12e1d5ae90a
yellowtoast <kjh9519@naver.com>
2022-06-18 20:35:56 +0900

    edit: edited start controller

--- withconi/lib/controller/start_controller.dart
+++ withconi/lib/controller/start_controller.dart
@@ -1,31 +1,30 @@
+import 'package:dartz/dartz.dart';
 import 'package:withconi/configs/constants/enum.dart';
-import 'package:withconi/controller/signup/user_data.dart';
+import 'package:withconi/controller/exception_controller.dart';
+import 'package:withconi/controller/signup/shared_data/user_data.dart';
+import 'package:withconi/core/error_handling/failures.dart';
 import 'package:withconi/data/repository/auth_repository.dart';
+import 'package:withconi/ui/widgets/snackbar.dart';
 import '../configs/constants/regex.dart';
 import '../configs/constants/strings.dart';
 import '../import_basic.dart';
+import 'signup/shared_data/conimal_data.dart';
 
-class StartPageController extends GetxController {
-  // ignore: unused_field
-  final UserData _userData = Get.put(UserData());
+class StartPageController extends GetxController with StateMixin<UserState> {
   final AuthRepository _repository = AuthRepository();
-  late RxString _email;
-  late RxBool isButtonValid;
-  late RxString buttonText;
-  late Rx<UserState> _userState;
+  final RxString _email = ''.obs;
+  final RxBool isButtonValid = false.obs;
+  final RxString buttonText = '다음'.obs;
+  final Rx<UserState> _userState = UserState.NONE.obs;
   RxnString emailErrorText = RxnString();
   TextEditingController emailTextController = TextEditingController();
   String nextRoute = '';
-
   String get email => _email.value;
 
   @override
   void onInit() {
     super.onInit();
-    _email = ''.obs;
-    isButtonValid = false.obs;
-    buttonText = '다음'.obs;
-    _userState = UserState.NONE.obs;
+    change(null, status: RxStatus.empty());
   }
 
   @override
@@ -40,22 +39,24 @@ class StartPageController extends GetxController {
   }
 
   signIn(ProviderOptions provider) async {
-    print(_userState.value);
+    late Either<Failure, UserState> userStateEither;
     switch (provider) {
       case ProviderOptions.GOOGLE:
-        _userState.value = await _repository.signInWithGoogle();
+        userStateEither = await _repository.signInWithGoogle();
         break;
       case ProviderOptions.KAKAO:
-        _userState.value = await _repository.signInWithKakao();
+        userStateEither = await _repository.signInWithKakao();
         break;
       case ProviderOptions.NAVER:
-        _userState.value = await _repository.signInWithNaver();
+        userStateEither = await _repository.signInWithNaver();
         break;
       case ProviderOptions.APPLE:
-        _userState.value = await _repository.signInWithApple();
+        userStateEither = await _repository.signInWithApple();
         break;
       default:
     }
+
+    changePageState(userStateEither);
   }
 
   validateEmail(_email) async {
@@ -69,27 +70,35 @@ class StartPageController extends GetxController {
       _userState.value = UserState.NONE;
     } else {
       emailErrorText.value = null;
-      _userState.value = await _repository.checkUserStateByEmail(
-          email: _email, provider: ProviderOptions.EMAIL);
+      checkUserState(_email);
     }
   }
 
+  checkUserState(String email) async {
+    change(null, status: RxStatus.loading());
+    Either<Failure, UserState> userStateEither = await _repository
+        .checkUserStateByEmail(email: email, provider: ProviderOptions.EMAIL);
+    changePageState(userStateEither);
+  }
+
   setNextStep(_userState) {
-    print('setNextStep 실행');
     switch (_userState) {
       case UserState.NONE:
         isButtonValid.value = false;
         buttonText.value = '다음';
+
         break;
       case UserState.SIGN_IN_EMAIL:
         isButtonValid.value = true;
         buttonText.value = '로그인';
         nextRoute = Routes.SIGNIN_EMAIL;
+
         break;
       case UserState.SIGN_UP_EMAIL:
         isButtonValid.value = true;
         buttonText.value = '회원가입';
         nextRoute = Routes.SIGNUP_PW;
+
         break;
       case UserState.SIGN_IN_SNS:
         //바로 가면 안되고 로그인 해야함 token이나 credential로
@@ -104,8 +113,24 @@ class StartPageController extends GetxController {
   }
 
   nextStep() {
+    final UserData _userData = Get.put(UserData());
+    final ConimalData _conimalData = Get.put(ConimalData());
     UserData.to.saveEmail(email);
-    _userState.value = UserState.NONE;
     Get.toNamed(nextRoute);
+  }
+
+  changePageState(Either<Failure, UserState> userStateEither) {
+    userStateEither.fold((failure) {
+      ExceptionController().mapFailureToDialog(failure);
+      change(UserState.NONE, status: RxStatus.empty());
+    }, (userState) {
+      if (userState == UserState.NONE) {
+        _userState.value = userState;
+        change(userState, status: RxStatus.empty());
+      } else {
+        _userState.value = userState;
+        change(userState, status: RxStatus.success());
+      }
+    });
   }
 }
