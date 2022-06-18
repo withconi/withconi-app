46872b8f7097aa5eadcecc45de4e6d0e5e7980d1
yellowtoast <kjh9519@naver.com>
2022-06-18 20:37:34 +0900

    edit: edited DiseaseSearchController

--- withconi/lib/controller/signup/disease_search_controller.dart
+++ withconi/lib/controller/signup/disease_search_controller.dart
@@ -1,36 +1,41 @@
+import 'package:dartz/dartz.dart';
+import 'package:withconi/controller/exception_controller.dart';
+import 'package:withconi/controller/signup/shared_data/disease_data.dart';
+import 'package:withconi/core/error_handling/exceptions.dart';
 import 'package:withconi/data/model/disease.dart';
-import 'package:withconi/data/repository/disease_search_repository.dart';
+import 'package:withconi/data/repository/disease_repository.dart';
+import '../../core/error_handling/failures.dart';
 import '../../import_basic.dart';
-import 'conimal_data.dart';
+import '../../ui/widgets/snackbar.dart';
+import 'shared_data/conimal_data.dart';
 
-class DiseaseSearchController extends GetxController {
+class DiseaseSearchController extends GetxController
+    with StateMixin<List<Disease>> {
   late RxString _disease;
   final DiseaseSearchRepository _diseaseRepository = DiseaseSearchRepository();
   TextEditingController diseaseTextController = TextEditingController();
-
+  RxBool listLoaded = false.obs;
   String get disease => _disease.value;
 
-  RxList<DiseaseModel> diseaseListSelected = RxList<DiseaseModel>();
-  RxList<DiseaseModel> diseaseListSearched = RxList<DiseaseModel>();
-  Set<DiseaseModel> resultSet = {};
-
-  Set<DiseaseModel> selectedSet = {};
-
-  Set<DiseaseModel> newResultSet = {};
+  RxList<Disease> diseaseListSelected = RxList<Disease>();
+  RxList<Disease> diseaseListSearched = RxList<Disease>();
+  Set<Disease> resultSet = {};
+  Set<Disease> selectedSet = {};
+  Set<Disease> newResultSet = {};
 
   @override
   void onInit() {
     super.onInit();
-
+    change(null, status: RxStatus.empty());
     _disease = ''.obs;
+    diseaseListSelected.assignAll(DiseaseData.to.selectedDiseases);
   }
 
   @override
   void onReady() {
     super.onReady();
 
-    debounce(_disease, searchNewDiseaseList,
-        time: const Duration(milliseconds: 100));
+    debounce(_disease, searchNewDiseaseList, time: Duration(milliseconds: 200));
   }
 
   void onDiseaseChanged(String val) {
@@ -41,20 +46,67 @@ class DiseaseSearchController extends GetxController {
   void clearResult() {
     diseaseTextController.clear();
     diseaseListSearched.clear();
+    change(null, status: RxStatus.empty());
   }
 
   searchNewDiseaseList(String val) async {
     if (val.isEmpty) {
+      clearResult();
       return;
     } else {
-      List<DiseaseModel> newResultList =
+      change(null, status: RxStatus.loading());
+
+      Either<Failure, List<Disease>> newResultListEither =
           await _diseaseRepository.getDiseaseList(diseaseName: val);
-      diseaseListSearched.assignAll(newResultList);
+
+      newResultListEither.fold(
+          (failure) => ExceptionController().mapFailureToDialog(failure),
+          (list) => diseaseListSearched.assignAll(list));
+
+      change(diseaseListSearched, status: RxStatus.success());
     }
   }
 
+  onDiseaseClicked(Disease disease) {
+    if (diseaseListSelected.contains(disease)) {
+      var diseaseRemoveEither = _diseaseRepository.removeDisease(disease);
+      diseaseRemoveEither.fold(
+          (fail) => ExceptionController().mapFailureToSnackbar(fail),
+          (success) => diseaseListSelected.remove(disease));
+    } else {
+      var diseaseAddEither = _diseaseRepository.addDisease(disease);
+      diseaseAddEither.fold(
+          (fail) => ExceptionController().mapFailureToSnackbar(fail),
+          (success) => diseaseListSelected.add(disease));
+    }
+    print(diseaseListSelected);
+  }
+
+  // Either<Failure, bool> addDisease(Disease disease) {
+  //   try {
+  //     DiseaseData.to.addDisease(disease);
+  //   } on MaxListException {
+  //     return Left(MaxListFailure(message: '질병은 4개까지만 추가 가능합니다'));
+  //   }
+  // }
+
+  // removeDisease(Disease disease) {
+  //   DiseaseData.to.removeDisease(disease);
+  // }
+
   saveDiseases() {
-    ConimalData.to.onDiseasesChange(diseaseListSelected.toList());
-    Get.back(result: diseaseListSelected.value.toList());
+    Get.back(result: getDiseaseInfo());
+  }
+
+  getDiseaseInfo() {
+    List<String> diseaseNameList = diseaseListSelected.map((element) {
+      return element.name;
+    }).toList();
+
+    Map<String, dynamic> diseaseInfo = {
+      'diseaseNameList': diseaseNameList,
+      'diseaseNum': diseaseNameList.length
+    };
+    return diseaseInfo;
   }
 }
