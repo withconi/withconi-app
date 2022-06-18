b57cb33ceff549c5cdd8ee82e8b501993e9f97ad
yellowtoast <kjh9519@naver.com>
2022-06-18 16:51:27 +0900

    fit: implemented exceptions class

--- withconi/lib/core/error_handling/exceptions.dart
+++ withconi/lib/core/error_handling/exceptions.dart
@@ -1,5 +1,64 @@
-class ServerException implements Exception {}
+import 'package:dio/dio.dart';
 
-class DataParsingException implements Exception {}
+class BadRequestException extends DioError implements Exception {
+  BadRequestException(RequestOptions r) : super(requestOptions: r);
 
-class NoConnectionException implements Exception {}
+  @override
+  String toString() {
+    return 'Invalid request';
+  }
+}
+
+class InternalServerErrorException extends DioError implements Exception {
+  InternalServerErrorException(RequestOptions r) : super(requestOptions: r);
+
+  @override
+  String toString() {
+    return 'Unknown error occurred, please try again later.';
+  }
+}
+
+class ConflictException extends DioError implements Exception {
+  ConflictException(RequestOptions r) : super(requestOptions: r);
+
+  @override
+  String toString() {
+    return 'Conflict occurred';
+  }
+}
+
+class UnauthorizedException extends DioError implements Exception {
+  UnauthorizedException(RequestOptions r) : super(requestOptions: r);
+
+  @override
+  String toString() {
+    return 'Access denied, wrong token';
+  }
+}
+
+class NotFoundException extends DioError implements Exception {
+  NotFoundException(RequestOptions r) : super(requestOptions: r);
+
+  @override
+  String toString() {
+    return 'The requested information could not be found';
+  }
+}
+
+class NoInternetConnectionException extends DioError implements Exception {
+  NoInternetConnectionException(RequestOptions r) : super(requestOptions: r);
+
+  @override
+  String toString() {
+    return 'No internet connection detected, please try again.';
+  }
+}
+
+class TimeOutException extends DioError implements Exception {
+  TimeOutException(RequestOptions r) : super(requestOptions: r);
+
+  @override
+  String toString() {
+    return 'The connection has timed out, please try again.';
+  }
+}
