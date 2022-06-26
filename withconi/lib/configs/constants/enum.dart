13e7b6191c2fd5e95d4e3374f20d0555dc30bc35
yellowtoast <kjh9519@naver.com>
2022-06-26 18:46:50 +0900

    add: add enum

--- withconi/lib/configs/constants/enum.dart
+++ withconi/lib/configs/constants/enum.dart
@@ -1,4 +1,34 @@
-// ignore_for_file: constant_identifier_names
+import 'package:json_annotation/json_annotation.dart';
 
-enum ProviderOptions { KAKAO, NAVER, GOOGLE, APPLE, EMAIL, NONE }
-enum CacheControllerKey { ACCESS_TOKEN, REFRESH_TOKEN, TOKEN_PROVIDER }
+enum Gender {
+  @JsonValue("female")
+  female,
+  @JsonValue("male")
+  male,
+}
+
+enum Species {
+  @JsonValue("cat")
+  cat,
+  @JsonValue("dog")
+  dog,
+}
+
+enum ProviderOptions {
+  @JsonValue("kakao")
+  kakao,
+  @JsonValue("naver")
+  naver,
+  @JsonValue("google")
+  google,
+  @JsonValue("apple")
+  apple,
+  @JsonValue("email")
+  email,
+  @JsonValue("none")
+  none,
+}
+
+enum CacheControllerKey { ACCESS_TOKEN, REFRESH_TOKEN, PROVIDER }
+
+enum UserState { SIGN_IN_EMAIL, SIGN_UP_EMAIL, SIGN_IN_SNS, SIGN_UP_SNS, NONE }
