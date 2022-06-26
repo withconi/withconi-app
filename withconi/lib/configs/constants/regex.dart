34992dd205b4ea25b1ec280ee569f68289cb21e3
yellowtoast <kjh9519@naver.com>
2022-06-26 18:47:04 +0900

    add: add some regexs

--- withconi/lib/configs/constants/regex.dart
+++ withconi/lib/configs/constants/regex.dart
@@ -4,5 +4,6 @@ class Regex {
       r"^([\w\.\_\-])*[a-zA-Z0-9]+([\w\.\_\-])*([a-zA-Z0-9])+([\w\.\_\-])+@([a-zA-Z0-9]+\.)+[a-zA-Z0-9]{2,8}$");
   static final password =
       RegExp(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$.!%*#?&]{8,}$");
-  static final name = RegExp(r"^[ㄱ-ㅎ|가-힣|a-z|A-Z]{1,8}$");
+  static final name = RegExp(r"^[ㄱ-ㅎ|가-힣|a-z|A-Z]{1,12}$");
+  static final nickName = RegExp(r"^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]{1,12}$");
 }
