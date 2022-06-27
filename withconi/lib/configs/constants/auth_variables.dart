a7850b3b56ba20a85e3198c022a4cf12299160d0
yellowtoast <kjh9519@naver.com>
2022-06-27 19:22:34 +0900

    edit: edit auth variables

--- withconi/lib/configs/constants/auth_variables.dart
+++ withconi/lib/configs/constants/auth_variables.dart
@@ -1,10 +1,10 @@
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:withconi/controller/auth_controller.dart';
 
-AuthController authController = AuthController.instance;
 final Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp();
-final FirebaseAuth auth = FirebaseAuth.instance;
+final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
