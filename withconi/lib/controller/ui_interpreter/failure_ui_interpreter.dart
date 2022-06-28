04ca91259c8a310779171c647e154f42a3801475
yellowtoast <kjh9519@naver.com>
2022-06-28 19:10:05 +0900

    add: add failure

---
+++ withconi/lib/controller/ui_interpreter/failure_ui_interpreter.dart
@@ -0,0 +1,39 @@
+import 'package:withconi/core/error_handling/error_message_object.dart';
+
+import '../../core/error_handling/failures.dart';
+import '../../ui/widgets/snackbar.dart';
+
+class FailureInterpreter {
+  FailureInterpreter._internal();
+  static final _singleton = FailureInterpreter._internal();
+  factory FailureInterpreter() => _singleton;
+
+  mapFailureToDialog(Failure _failure) {
+    ErrorMessage errorMessage =
+        ErrorMessage.mapFailureToErrorMessage(failure: _failure);
+
+    print(
+      'FAIL[$_failure] => title: ${errorMessage.title} // message: ${errorMessage.message}',
+    );
+
+    return showSnackbar(text: errorMessage.message);
+  }
+
+  mapFailureToSnackbar(Failure _failure) {
+    ErrorMessage errorMessage =
+        ErrorMessage.mapFailureToErrorMessage(failure: _failure);
+    print(
+      'FAIL[$_failure] => title: ${errorMessage.title} // message: ${errorMessage.message}',
+    );
+    return showSnackbar(text: errorMessage.message);
+  }
+
+  mapFailureToPage(Failure _failure) {
+    ErrorMessage errorMessage =
+        ErrorMessage.mapFailureToErrorMessage(failure: _failure);
+    print(
+      'FAIL[$_failure] => title: ${errorMessage.title} // message: ${errorMessage.message}',
+    );
+    return showSnackbar(text: errorMessage.message);
+  }
+}
