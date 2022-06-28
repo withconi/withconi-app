206cf58164b0b2f31db95c39aa6bb3379dc0f4a1
yellowtoast <kjh9519@naver.com>
2022-06-28 19:09:49 +0900

    edit: edit user repository

--- withconi/lib/data/repository/user_repository.dart
+++ withconi/lib/data/repository/user_repository.dart
@@ -1,84 +1,27 @@
-import 'dart:io';
+import 'dart:async';
 import 'package:dartz/dartz.dart';
-import 'package:withconi/configs/constants/enum.dart';
-import 'package:withconi/data/repository/conimal_repository.dart';
-
-import '../../controller/failure_ui_interpreter.dart';
-import '../../controller/signup/shared_data/user_data.dart';
-import '../../core/auth_info.dart';
+import 'package:withconi/core/error_handling/exceptions.dart';
+import 'package:withconi/data/provider/user_api.dart';
 import '../../core/error_handling/failures.dart';
-import '../model/conimal.dart';
 import '../model/user.dart';
 
 class UserRepository {
-  UserRepository._internal();
-  static final _singleton = UserRepository._internal();
-  factory UserRepository() => _singleton;
+  final UserAPI _api = UserAPI();
 
-  ConimalRepository _conimalRepository = ConimalRepository();
-
-  uploadProfileImage() {}
-
-  deleteProfileImage() {}
-
-  editProfileImage() {}
-
-  String getUserEmail() {
-    return UserData.to.email;
-  }
-
-  getUserName() {
-    return UserData.to.name;
-  }
-
-  getProvider() {
-    return UserData.to.provider;
-  }
-
-  saveUserEmail(String email) {
-    UserData.to.saveEmail(email);
-  }
-
-  saveUserAuthInfo(AuthInfo authInfo) {
-    UserData.to.saveAuthInfo(authInfo);
-  }
-
-  saveUserName(String name) {
-    UserData.to.saveName(name);
-  }
-
-  saveUserPassword(String password) {
-    UserData.to.savePassword(password);
-  }
-
-  saveUserNickname(String nickName) {
-    UserData.to.saveNickName(nickName);
-  }
-
-  saveUserProfile(File? profileFile) {
-    UserData.to.saveProfileSrc(profileFile);
-  }
-
-  saveProvider(ProviderOptions provider) {
-    UserData.to.saveProvider(provider);
-  }
-
-  registerUserDB() {}
-
-  WcUser? newUser() {
-    Either<Failure, List<Conimal>> allConimalEither =
-        _conimalRepository.getAllTempConimal();
-    allConimalEither.fold(
-        (fail) => FailureInterpreter().mapFailureToDialog(fail), (conimals) {
-      WcUser newUser = WcUser(
-          uid: DateTime.now().millisecondsSinceEpoch.toString(),
-          email: UserData.to.email,
-          displayName: UserData.to.name,
-          nickname: UserData.to.nickName,
-          provider: UserData.to.provider,
-          photoURL: UserData.to.profileImg?.path,
-          conimals: conimals);
-      return newUser;
-    });
+  Future<Either<Failure, WcUser>> getUserInfoByUid(
+      {required String uid}) async {
+    try {
+      Map<String, dynamic> data = await _api.getUserInfo(uid: uid);
+      WcUser wcUser = WcUser.fromJson(data);
+      return Right(wcUser);
+    } on NoInternetConnectionException {
+      return Left(NoConnectionFailure());
+    } on DataParsingException {
+      return Left(DataParsingFailure());
+    } on NotFoundException {
+      return Left(NotFoundFailure());
+    } catch (e) {
+      return Left(NoUserDataFailure());
+    }
   }
 }
