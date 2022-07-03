44187e1e01b04aa85352e626f56978204da252e7
yellowtoast <kjh9519@naver.com>
2022-07-03 23:08:19 +0900

    edit: edit auth Controller

--- withconi/lib/controller/auth_controller.dart
+++ withconi/lib/controller/auth_controller.dart
@@ -1,38 +1,62 @@
+import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:withconi/configs/constants/enum.dart';
-import 'package:withconi/configs/constants/auth_variables.dart';
+import 'package:withconi/configs/helpers/token_manager.dart';
+import 'package:withconi/controller/signup/shared_data/conimal_data.dart';
+import 'package:withconi/controller/signup/shared_data/user_data.dart';
+import 'package:withconi/controller/ui_interpreter/failure_ui_interpreter.dart';
+import 'package:withconi/core/error_handling/failures.dart';
 import 'package:withconi/data/repository/auth_repository.dart';
-import 'package:withconi/ui/pages/start/start_page.dart';
+import 'package:withconi/data/repository/user_repository.dart';
+import '../configs/constants/auth_variables.dart';
+import '../data/model/user.dart';
 import '../import_basic.dart';
 
 class AuthController extends GetxController {
-  static AuthController instance = Get.find();
+  static AuthController get to => Get.find();
   final AuthRepository _authRepository = AuthRepository();
-  final Rx<ProviderOptions> provider = ProviderOptions.NONE.obs;
+  final UserRepository _userRepository = UserRepository();
   final RxBool _isLoggedIn = false.obs;
-
-  @override
-  onInit() {
-    super.onInit();
-    provider.value = _authRepository.getAuthTokenProvider();
-  }
+  final Rxn<WcUser> wcUser = Rxn<WcUser>();
 
   @override
   onReady() async {
     super.onReady();
-    _isLoggedIn.value =
-        await _authRepository.isUserLoggedIn(provider: provider.value);
-    await Future.delayed(const Duration(milliseconds: 1300), () {
-      _setInitialPage(_isLoggedIn.value);
+    await Future.delayed(const Duration(milliseconds: 1300), () async {
+      await setUserState(firebaseAuth.currentUser);
     });
   }
 
-  _setInitialPage(bool _isLoggedIn) {
-    if (_isLoggedIn) {
-      // Get.offAll(() => const HomePage());
+  setUserState(User? firebaseUser) async {
+    _isLoggedIn.value = await _authRepository.isUserLoggedIn();
+    _setUserInfo(_isLoggedIn.value);
+  }
+
+  _setUserInfo(bool isLoggedIn) async {
+    if (isLoggedIn) {
+      Either<Failure, WcUser> wcUserEither = await _userRepository
+          .getUserInfoWithUid(uid: firebaseAuth.currentUser!.uid);
+      wcUserEither.fold((fail) {
+        wcUser.value = null;
+        FailureInterpreter().mapFailureToDialog(fail);
+      }, (user) {
+        wcUser.value = user;
+      });
     } else {
-      Get.offAllNamed(Routes.START);
+      wcUser.value = null;
     }
+    _setInitialPage(wcUser.value);
+  }
+
+  _setInitialPage(WcUser? wcUser) {
+    if (wcUser == null) {
+      Get.offAllNamed(Routes.START);
+    } else {
+      Get.offAllNamed(Routes.HOME);
+    }
+  }
+
+  signOut() {
+    _authRepository.signOut();
   }
 }
