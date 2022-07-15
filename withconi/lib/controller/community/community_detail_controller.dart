95ef958a84ace42cd798be6114a2526081dd56b9
yellowtoast <kjh9519@naver.com>
2022-07-15 17:15:15 +0900

    feat: implement community detail controller

--- withconi/lib/controller/community/community_detail_controller.dart
+++ withconi/lib/controller/community/community_detail_controller.dart
@@ -1,19 +1,14 @@
+import 'package:withconi/controller/infinite_scroll_controller.dart';
+import 'package:withconi/controller/ui_interpreter/failure_ui_interpreter.dart';
+import 'package:withconi/data/repository/community_repository.dart';
+
+import '../../data/model/post.dart';
 import '../../import_basic.dart';
 import '../ui_helper/infinite_scroll.dart';
 
 class CommunityDetailController extends GetxController {
-  RxList<String> postList = [
-    '먼지',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'H',
-    'I',
-    'b',
-  ].obs;
+  CommunityRepository _communityRepository = CommunityRepository();
+  RxList<Post> postList = <Post>[].obs;
 
   List<String> postType = [
     '모두',
@@ -24,34 +19,50 @@ class CommunityDetailController extends GetxController {
   RxString selectedPostSort = '최신순'.obs;
   List<String> postSort = ['최신순', '인기순'];
   RxList<String> userLikedPost = ['먼지', 'c', 'd'].obs;
-  Rx<ScrollController> scrollController = ScrollController().obs;
+
+  final _paginationFilter = PaginationFilter().obs;
+  final _lastPage = false.obs;
+
+  int get limit => _paginationFilter.value.limit!;
+  int get _page => _paginationFilter.value.page!;
+  bool get lastPage => _lastPage.value;
 
   @override
   onInit() {
+    ever(_paginationFilter, (_) => _getPostList());
+    _changePaginationFilter(1, 15);
     super.onInit();
-
-    // scrollController.value.addListener(() {
-
-    //   scrollController.value.
-    // });
   }
 
-  @override
-  onReady() {
-    super.onReady();
+  Future<void> _getPostList() async {
+    final postDataEither = await _communityRepository.getPostList(
+        paginationFilter: _paginationFilter.value);
 
-    // ever(scrollController, getPostList);
+    postDataEither.fold(
+        (fail) => FailureInterpreter()
+            .mapFailureToSnackbar(fail, '_getPostListByPage'), (newPostList) {
+      if (newPostList.isEmpty) {
+        _lastPage.value = true;
+      } else {
+        postList.addAll(newPostList);
+      }
+    });
   }
 
-  Future<List<String>> getPost() async {
-    List<String> postList = ['a', 'b'];
-    return await postList;
+  void changeTotalPerPage(int limitValue) {
+    postList.clear();
+    _lastPage.value = false;
+    _changePaginationFilter(1, limitValue);
   }
 
-  // getPostList(_scrollController) async {
-  //   postList.add(await infiniteScroller.getDataByScrollEvent(
-  //       asyncFunction: getPost, scrollController: _scrollController));
-  // }
+  void _changePaginationFilter(int page, int limit) {
+    _paginationFilter.update((val) {
+      val!.page = page;
+      val.limit = limit;
+    });
+  }
+
+  void loadNextPage() => _changePaginationFilter(_page + 1, limit);
 
   void onPostSortingChanged(String? sortingType) {
     selectedPostSort.value = sortingType!;
