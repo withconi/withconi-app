4fbe87bb5378ec34e6eceb6b40d5b836aee93d64
yellowtoast <kjh9519@naver.com>
2022-07-17 19:10:49 +0900

    feat: implement conimal API

---
+++ withconi/lib/data/provider/conimal_api.dart
@@ -0,0 +1,15 @@
+import 'package:withconi/core/network_handling/network_service.dart';
+import '../../configs/constants/api_url.dart';
+
+class ConimalAPI {
+  final Api _dio = Api();
+
+  Future<String?> createConimal({required Map<String, dynamic> conimal}) async {
+    Map<String, dynamic> data = await _dio.apiCall(
+      url: HttpUrl.CREATE_CONIMAL,
+      queryParameters: null,
+      body: conimal,
+      requestType: RequestType.POST,
+    );
+  }
+}
