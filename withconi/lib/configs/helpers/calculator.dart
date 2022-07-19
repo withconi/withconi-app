ae564f92d714e8bf182b94704b94eda40500e01f
yellowtoast <kjh9519@naver.com>
2022-07-19 23:18:20 +0900

    add: add date calculator

--- withconi/lib/configs/helpers/calculator.dart
+++ withconi/lib/configs/helpers/calculator.dart
@@ -1,5 +1,5 @@
-class Calculator {
-  calculateAge(DateTime birthDate) {
+class TimeCalculator {
+  int calculateAge(DateTime birthDate) {
     DateTime currentDate = DateTime.now();
     int age = currentDate.year - birthDate.year;
     int month1 = currentDate.month;
@@ -14,5 +14,27 @@ class Calculator {
       }
     }
     return age;
+  }
+
+  String calculateUploadAt(DateTime uploadAt) {
+    List<String> timeSuffix = ['방금', '분', '시간', '일', '달', '년'];
+    DateTime currentDate = DateTime.now();
+    String uploadAtStr = '';
+    Duration differDuration = currentDate.difference(uploadAt);
+    if (differDuration.inMinutes < 1) {
+      uploadAtStr = '${timeSuffix[0]} 전';
+    } else if (differDuration.inMinutes < 60) {
+      uploadAtStr = '${differDuration.inMinutes}${timeSuffix[1]} 전';
+    } else if (differDuration.inMinutes < (60 * 24)) {
+      uploadAtStr = '${differDuration.inHours}${timeSuffix[2]} 전';
+    } else if (differDuration.inMinutes < (60 * 24 * 30)) {
+      uploadAtStr = '${differDuration.inDays}${timeSuffix[3]} 전';
+    } else if (differDuration.inDays < 365) {
+      uploadAtStr = '${differDuration.inDays}${timeSuffix[4]} 전';
+    } else if (differDuration.inDays >= 365) {
+      uploadAtStr = '${differDuration.inDays % 365}${timeSuffix[5]} 전';
+    }
+
+    return uploadAtStr;
   }
 }
