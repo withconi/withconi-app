5e0ea57d0313241f4ea523597d8b2f77553988cf
yellowtoast <kjh9519@naver.com>
2022-07-20 15:06:34 +0900

    add: add communityRepository features

--- withconi/lib/data/repository/community_repository.dart
+++ withconi/lib/data/repository/community_repository.dart
@@ -7,19 +7,36 @@ import 'package:withconi/core/error_handling/exceptions.dart';
 import 'package:withconi/data/model/disease.dart';
 import 'package:withconi/data/provider/community_api.dart';
 import 'package:withconi/data/provider/disease_api.dart';
+import '../../configs/constants/enum.dart';
 import '../../core/error_handling/failures.dart';
 import '../../core/network_handling/response_model.dart';
+import '../model/board.dart';
 import '../model/post.dart';
 
 class CommunityRepository {
   final CommunityAPI _api = CommunityAPI();
 
+  Future<Either<Failure, List<Board>>> getBoardList() async {
+    try {
+      Map<String, dynamic> data = await _api.getBoards();
+      List<Board> boardList = BoardResponse.fromJson(data).results;
+      return Right(boardList);
+    } on NoInternetConnectionException {
+      return Left(NoConnectionFailure());
+    } on DataParsingException {
+      return Left(DataParsingFailure());
+    }
+  }
+
   Future<Either<Failure, List<Post>>> getPostList(
       {required PaginationFilter paginationFilter,
-      required String boardId}) async {
+      required String boardId,
+      required PostType postType}) async {
     try {
       Map<String, dynamic> data = await _api.getPosts(
-          paginationFilter: paginationFilter, boardId: boardId);
+          paginationFilter: paginationFilter,
+          boardId: boardId,
+          postType: postTypeToValue(postType));
       List<Post> postList = PostResponse.fromJson(data).results;
       return Right(postList);
     } on NoInternetConnectionException {
