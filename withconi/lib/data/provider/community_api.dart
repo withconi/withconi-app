2ddbb5eb5cbac16465494465c891112e937530e1
yellowtoast <kjh9519@naver.com>
2022-07-20 15:05:57 +0900

    add: add community api

--- withconi/lib/data/provider/community_api.dart
+++ withconi/lib/data/provider/community_api.dart
@@ -6,15 +6,28 @@ import '../model/post.dart';
 class CommunityAPI {
   final Api _dio = Api();
 
-  Future<Map<String, dynamic>> getPosts(
-      {required PaginationFilter paginationFilter,
-      required String boardId}) async {
+  Future<Map<String, dynamic>> getBoards() async {
+    Map<String, dynamic> boardsData = await _dio.apiCall(
+      url: HttpUrl.BOARD_GET,
+      queryParameters: null,
+      body: null,
+      requestType: RequestType.GET,
+    );
+    return boardsData;
+  }
+
+  Future<Map<String, dynamic>> getPosts({
+    required PaginationFilter paginationFilter,
+    required String boardId,
+    required String postType,
+  }) async {
     Map<String, dynamic> postsData = await _dio.apiCall(
-      url: HttpUrl.GET_POST,
+      url: HttpUrl.POST_GET,
       queryParameters: {
         "page": paginationFilter.page,
         "listSize": paginationFilter.limit,
-        "boardId": boardId
+        "boardId": boardId,
+        "speciesType": postType,
       },
       body: null,
       requestType: RequestType.GET,
@@ -28,7 +41,7 @@ class CommunityAPI {
       String? speciesType,
       String? searchType}) async {
     Map<String, dynamic> postsData = await _dio.apiCall(
-      url: HttpUrl.CREATE_CONIMAL,
+      url: HttpUrl.CONIMAL_CREATE,
       queryParameters: {
         "page": paginationFilter.page,
         "listSize": paginationFilter.limit,
@@ -44,7 +57,7 @@ class CommunityAPI {
   Future<Map<String, dynamic>> newPost(
       {required Map<String, dynamic> newPostJson}) async {
     Map<String, dynamic> postedData = await _dio.apiCall(
-      url: HttpUrl.CREATE_POST,
+      url: HttpUrl.POST_CREATE,
       queryParameters: null,
       body: newPostJson,
       requestType: RequestType.POST,
