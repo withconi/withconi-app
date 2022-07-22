7c3cd89c13edb6af6114e37714212e32a2fa224b
yellowtoast <kjh9519@naver.com>
2022-07-22 21:14:43 +0900

    feat: implement textfield

--- withconi/lib/ui/widgets/text_field/textfield.dart
+++ withconi/lib/ui/widgets/text_field/textfield.dart
@@ -6,33 +6,37 @@ class WcTextField extends StatelessWidget {
   WcTextField({
     Key? key,
     required this.hintText,
-    required this.onChanged,
-    required this.textController,
+    this.onChanged,
+    this.textController,
     required this.keyboardType,
     this.textObscure = false,
     this.enabled = true,
     this.errorText,
     this.labelText,
     this.width = double.infinity,
+    this.readOnly = false,
   }) : super(key: key);
 
   String hintText;
   void Function(String)? onChanged;
-  TextEditingController textController;
+  TextEditingController? textController;
   String? labelText;
   TextInputType keyboardType;
   bool textObscure;
   bool enabled;
   String? errorText;
   double? width;
+  bool readOnly;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: width,
+      height: 61,
       child: TextField(
+        readOnly: readOnly,
         autofocus: false,
-        enabled: enabled,
+        enabled: !readOnly,
         obscuringCharacter: '●',
         obscureText: textObscure,
         controller: textController,
@@ -43,7 +47,7 @@ class WcTextField extends StatelessWidget {
         cursorColor: WcColors.blue100,
         decoration: InputDecoration(
           isDense: true,
-          contentPadding: EdgeInsets.only(top: 10, bottom: 8),
+          contentPadding: EdgeInsets.only(top: 11, bottom: 10),
           errorText: errorText,
           labelText: labelText,
           labelStyle: GoogleFonts.notoSans(
@@ -51,7 +55,10 @@ class WcTextField extends StatelessWidget {
               fontSize: 17,
               fontWeight: FontWeight.w300),
           floatingLabelStyle: GoogleFonts.notoSans(
-              color: WcColors.black, fontSize: 17, fontWeight: FontWeight.w500),
+              color: WcColors.black,
+              fontSize: 17,
+              height: 0.55,
+              fontWeight: FontWeight.w500),
           alignLabelWithHint: true,
           enabledBorder: const UnderlineInputBorder(
             borderSide: BorderSide(color: WcColors.grey100, width: 0.6),
