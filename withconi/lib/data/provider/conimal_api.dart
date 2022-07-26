6a3c2b77169423db62069a2a4edf9afeb77156b8
yellowtoast <kjh9519@naver.com>
2022-07-26 22:03:16 +0900

    edit:  edit conimal api

--- withconi/lib/data/provider/conimal_api.dart
+++ withconi/lib/data/provider/conimal_api.dart
@@ -1,3 +1,4 @@
+import 'package:withconi/controller/auth_controller.dart';
 import 'package:withconi/core/network_handling/network_service.dart';
 import '../../configs/constants/api_url.dart';
 
@@ -9,6 +10,26 @@ class ConimalAPI {
       url: HttpUrl.CONIMAL_CREATE,
       queryParameters: null,
       body: conimal,
+      requestType: RequestType.POST,
+    );
+  }
+
+  Future<String?> updateConimal({required Map<String, dynamic> conimal}) async {
+    print({...conimal});
+    Map<String, dynamic> data = await _dio.apiCall(
+      url: HttpUrl.CONIMAL_UPDATE,
+      queryParameters: null,
+      body: conimal,
+      requestType: RequestType.POST,
+    );
+  }
+
+  Future<String?> deleteConimal(
+      {required String uid, required String conimalId}) async {
+    Map<String, dynamic> data = await _dio.apiCall(
+      url: HttpUrl.CONIMAL_DELETE,
+      queryParameters: null,
+      body: {"uid": uid, "conimalId": conimalId},
       requestType: RequestType.POST,
     );
   }
