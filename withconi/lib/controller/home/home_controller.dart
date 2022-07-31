602ce5a76f908a6a4e3928fcd3bfe984f158a202
yellowtoast <kjh9519@naver.com>
2022-07-31 23:11:57 +0900

    edit: edit home controller

--- withconi/lib/controller/home/home_controller.dart
+++ withconi/lib/controller/home/home_controller.dart
@@ -1,24 +1,32 @@
 import 'package:get/get.dart';
 import 'package:withconi/controller/auth_controller.dart';
+import 'package:withconi/data/repository/conimal_repository.dart';
+import 'package:withconi/ui/widgets/loading.dart';
+import '../../configs/constants/auth_variables.dart';
 import '../../data/model/conimal.dart';
 import '../../data/model/disease.dart';
 import '../../data/model/user.dart';
+import '../../routes/withconi_routes.dart';
 
 class HomeController extends GetxController {
-  Rxn<WcUser> _wcUser = Rxn<WcUser>();
+  ConimalRepository _conimalRepository = ConimalRepository();
+  late WcUser _wcUser;
   RxList<Conimal> conimalList = RxList<Conimal>();
   Rxn<Conimal> selectedConimal = Rxn<Conimal>();
   RxInt selectedConimalIndex = 0.obs;
   RxInt daysAfterAdoption = 0.obs;
   RxInt conimalCount = 0.obs;
   RxList<Disease> allDiseasesList = RxList<Disease>();
+
   RxInt navIndex = 0.obs;
-  WcUser? get wcUser => _wcUser.value;
+  WcUser? get wcUser => _wcUser;
+  RxBool isExpansionOneOpened = false.obs;
   RxBool isExpansionTwoOpened = false.obs;
 
   @override
   onInit() {
     super.onInit();
+    selectedConimalIndex.value = 0;
     _setWcUser();
     _setConimalList();
     _setSelectedConimal(selectedConimalIndex.value);
@@ -26,11 +34,11 @@ class HomeController extends GetxController {
   }
 
   void _setWcUser() {
-    _wcUser.value = Get.arguments as WcUser;
+    _wcUser = AuthController.to.wcUser.value!;
   }
 
   void _setConimalList() {
-    conimalList.assignAll(_wcUser.value!.conimals);
+    conimalList.assignAll(_wcUser.conimals);
     conimalCount.value = conimalList.length;
   }
 
@@ -41,16 +49,36 @@ class HomeController extends GetxController {
         daysBetween(selectedConimal.value!.adoptedDate, DateTime.now());
   }
 
+  Future<void> refreshPage() async {
+    await AuthController.to.refreshUserInfo();
+    onInit();
+  }
+
   void _setAllDiseases() {
     Set<Disease> diseaseSet = {};
     for (Conimal conimal in conimalList) {
       diseaseSet.addAll(conimal.diseases.toSet());
     }
-    allDiseasesList.addAll(diseaseSet.toList());
+    allDiseasesList.assignAll(diseaseSet.toList());
+    allDiseasesList.refresh();
   }
 
   void onSelectedConimalChanged(int index) {
     _setSelectedConimal(index);
+  }
+
+  addDisease({required Conimal selectedConimal}) async {
+    List<Disease>? selectedDiseases = await Get.toNamed(Routes.DISEASE_ADD,
+        arguments: selectedConimal.diseases) as List<Disease>?;
+    if (selectedDiseases == null) {
+    } else {
+      await showLoading(() => _conimalRepository.updateConimalDisease(
+          conimalId: selectedConimal.conimalId,
+          diseases: selectedDiseases,
+          userId: selectedConimal.userId!));
+
+      refreshPage();
+    }
   }
 
   int daysBetween(DateTime from, DateTime to) {
