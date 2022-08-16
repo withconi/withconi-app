8e06c137a93391e482eacf5073b12abe57776867
yellowtoast <kjh9519@naver.com>
2022-08-16 23:14:30 +0900

    edit: edit splash page

--- withconi/lib/ui/pages/splash.dart
+++ withconi/lib/ui/pages/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:withconi/controller/auth_controller.dart';
