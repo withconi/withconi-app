747bacf72856b81d0bfb33485e18308d64b9e6f7
yellowtoast <kjh9519@naver.com>
2022-08-27 20:24:08 +0900

    add: email verification controller

--- withconi/lib/controller/common_controller/email_verification_controller.dart
+++ withconi/lib/controller/common_controller/email_verification_controller.dart
@@ -1,6 +1,7 @@
 import 'package:withconi/controller/auth_controller.dart';
 import 'package:withconi/data/repository/auth_repository.dart';
 import 'package:withconi/data/repository/signup_repository.dart';
+import 'package:withconi/ui/widgets/loading.dart';
 
 import '../../../import_basic.dart';
 
@@ -12,19 +13,27 @@ class EmailVerificationController extends GetxController {
   String get email => _email;
 
   @override
-  void onInit() {
+  Future<void> onInit() async {
     super.onInit();
     _email = AuthController.to.wcUser.value!.email;
+    await sendVerificationEmail(email: email, currentPath: Get.currentRoute);
   }
 
-  sendVerificationEmail() async {
-    var result =
-        await _authRepository.sendVerificationEmail(email: 'asdfd@adsfd.com');
+  sendVerificationEmail(
+      {required String email, required String currentPath}) async {
+    var result = await _authRepository.sendVerificationEmail(
+        email: email, currentPath: currentPath);
     result!.fold((l) => null, (r) => null);
   }
 
-  checkEmailVerification({required String email}) async {
-    var result = await _authRepository.sendVerificationEmail(email: email);
+  // checkEmailVerification({required String email}) async {
+  //   var result = await _authRepository.sendVerificationEmail(email: email,currentPath: currentPath);
+  //   result!.fold((l) => null, (r) => null);
+  // }
+
+  resendVerificationEmail() async {
+    var result = await showLoading(() => _authRepository.sendVerificationEmail(
+        email: email, currentPath: Get.currentRoute));
     result!.fold((l) => null, (r) => null);
   }
 }
