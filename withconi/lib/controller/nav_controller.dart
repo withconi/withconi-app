e4ae3f438eb670fbb6f505862bdb294a5174c09f
yellowtoast <kjh9519@naver.com>
2022-09-04 23:10:25 +0900

    edit: edit navigation controller

--- withconi/lib/controller/nav_controller.dart
+++ withconi/lib/controller/nav_controller.dart
@@ -5,29 +5,33 @@ import '../../data/model/disease.dart';
 import '../../data/model/user.dart';
 import '../routes/withconi_routes.dart';
 
-class NavController extends GetxController {
-  static NavController get to => Get.find();
+class NavigationController extends GetxController {
+  static NavigationController get to => Get.find();
   RxInt navBarIndex = 0.obs;
 
-  onNavChanged({required int navIndex}) {
-    switch (navIndex) {
-      case 0:
-        Get.offAllNamed(Routes.HOME, arguments: AuthController.to.wcUser.value);
-        break;
-      case 1:
-        Get.offAllNamed(Routes.DIAGNOSIS_MAIN);
-        break;
-      case 2:
-        Get.offAllNamed(Routes.HOSPITAL_MAIN);
-        break;
-      case 3:
-        Get.offAllNamed(Routes.COMMUNITY_MAIN);
-        break;
-      case 4:
-        Get.offAllNamed(Routes.DICTIONARY_MAIN);
-        break;
-      default:
-    }
-    navBarIndex.value = navIndex;
+  // onNavChanged({required int navIndex}) {
+  //   switch (navIndex) {
+  //     case 0:
+  //       Get.offAllNamed(Routes.HOME, arguments: AuthController.to.wcUser.value);
+  //       break;
+  //     case 1:
+  //       Get.offAllNamed(Routes.DIAGNOSIS_MAIN);
+  //       break;
+  //     case 2:
+  //       Get.offAllNamed(Routes.MAP_MAIN);
+  //       break;
+  //     case 3:
+  //       Get.offAllNamed(Routes.COMMUNITY_MAIN);
+  //       break;
+  //     case 4:
+  //       Get.offAllNamed(Routes.DICTIONARY_MAIN);
+  //       break;
+  //     default:
+  //   }
+  //   navBarIndex.value = navIndex;
+  // }
+
+  void changeNavIndex(int index) {
+    navBarIndex(index);
   }
 }
