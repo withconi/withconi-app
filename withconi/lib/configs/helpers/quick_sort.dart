983698bd9ec68ce25224bb0ead5230d6bf240033
yellowtoast <kjh9519@naver.com>
2022-09-16 23:13:17 +0900

    feat: implement quick sort

--- withconi/lib/configs/helpers/quick_sort.dart
+++ withconi/lib/configs/helpers/quick_sort.dart
@@ -1,42 +1,79 @@
-// import 'package:withconi/ui/entities/custom_marker.dart';
+import 'package:withconi/data/model/abstract_class/place_type.dart';
+import 'package:withconi/ui/entities/custom_marker.dart';
 
-// class QuickSort {
-//   sortPlaceListByDistance({required List<CustomMarker> placeMarkerList}) {
-//     int high = placeMarkerList.length - 1;
-//     int low = 0;
-//     List<CustomMarker> result = _quickSort(placeMarkerList, low, high);
+import '../constants/enum.dart';
 
-//     return result;
-//   }
+class QuickSort {
+  sortPlaceByType(
+      {required List<CustomMarker> placeMarkerList,
+      required SortType sortType}) {
+    int high = placeMarkerList.length - 1;
+    int low = 0;
+    List<CustomMarker> result =
+        _quickSort(placeMarkerList, low, high, sortType);
 
-//   List<CustomMarker> _quickSort(List<CustomMarker> list, int low, int high) {
-//     if (low < high) {
-//       int pi = partition(list, low, high);
-//       _quickSort(list, low, pi - 1);
-//       _quickSort(list, pi + 1, high);
-//     }
-//     return list;
-//   }
+    return result;
+  }
 
-//   int partition(List<CustomMarker> list, low, high) {
-//     if (list.isEmpty) {
-//       return 0;
-//     }
-//     double pivot = list[high].distance;
-//     int i = low - 1;
-//     for (int j = low; j < high; j++) {
-//       if (list[j].distance < pivot) {
-//         i++;
-//         swap(list, i, j);
-//       }
-//     }
-//     swap(list, i + 1, high);
-//     return i + 1;
-//   }
+  // sortPlaceByVisiting({required List<CustomMarker> placeMarkerList}) {
+  //   int high = placeMarkerList.length - 1;
+  //   int low = 0;
+  //   List<CustomMarker> result = _quickSort(placeMarkerList, low, high);
 
-//   void swap(List<CustomMarker> list, int i, int j) {
-//     CustomMarker temp = list[i];
-//     list[i] = list[j];
-//     list[j] = temp;
-//   }
-// }
+  //   return result;
+  // }
+
+  List<CustomMarker> _quickSort(
+      List<CustomMarker> list, int low, int high, SortType sortType) {
+    if (low < high) {
+      late int pi;
+
+      if (sortType == SortType.visiting) {
+        pi = visitingPartition(list, low, high);
+      } else if (sortType == SortType.nearest) {
+        pi = distancePartition(list, low, high);
+      }
+      _quickSort(list, low, pi - 1, sortType);
+      _quickSort(list, pi + 1, high, sortType);
+    }
+    return list;
+  }
+
+  int distancePartition(List<CustomMarker> list, low, high) {
+    if (list.isEmpty) {
+      return 0;
+    }
+    double pivot = list[high].place.distanceByMeter;
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      if (list[j].place.distanceByMeter < pivot) {
+        i++;
+        swap(list, i, j);
+      }
+    }
+    swap(list, i + 1, high);
+    return i + 1;
+  }
+
+  int visitingPartition(List<CustomMarker> list, low, high) {
+    if (list.isEmpty) {
+      return 0;
+    }
+    int pivot = list[high].place.totalVisitingConimal;
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      if (list[j].place.totalVisitingConimal < pivot) {
+        i++;
+        swap(list, i, j);
+      }
+    }
+    swap(list, i + 1, high);
+    return i + 1;
+  }
+
+  void swap(List<CustomMarker> list, int i, int j) {
+    CustomMarker temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+  }
+}
