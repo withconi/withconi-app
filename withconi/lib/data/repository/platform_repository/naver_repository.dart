9f64995140c827a916f8f9cdd7e946d2d60477a5
yellowtoast <kjh9519@naver.com>
2022-09-28 22:21:17 +0900

    feat: implement sns provier repositories

---
+++ withconi/lib/data/repository/platform_repository/naver_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:withconi/data/provider/kakao_api.dart';
+import 'package:withconi/data/provider/naver_api.dart';
+
+import '../../../core/error_handling/exceptions.dart';
+import '../../../core/error_handling/failures.dart';
+
+class NaverRepository {
+  final NaverAPI _api = NaverAPI();
+
+  Future<Either<Failure, String>> getNaverToken() async {
+    try {
+      String naverAccessToken = await _api.getNaverToken();
+      return Right(naverAccessToken);
+    } on NoInternetConnectionException {
+      return Left(NoConnectionFailure());
+    } on PlatformException {
+      return Left(NoUserDataFailure());
+    } catch (e) {
+      return Left(NoUserDataFailure());
+    }
+  }
+}
