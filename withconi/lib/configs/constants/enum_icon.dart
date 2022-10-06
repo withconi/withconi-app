2355fa1e8fd8bf549f890127fccb03e8be174f6c
yellowtoast <kjh9519@naver.com>
2022-10-06 18:46:32 +0900

    feat: implement enum icon

---
+++ withconi/lib/configs/constants/enum_icon.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_svg/svg.dart';
+
+import 'enum.dart';
+
+SvgPicture moreOptionsToIcon(MoreOption option) {
+  switch (option) {
+    case MoreOption.block:
+      return SvgPicture.asset('assets/icons/circle_block.svg');
+    case MoreOption.report:
+      return SvgPicture.asset('assets/icons/circle_report.svg');
+    case MoreOption.edit:
+      return SvgPicture.asset('assets/icons/circle_edit.svg');
+    case MoreOption.delete:
+      return SvgPicture.asset('assets/icons/circle_delete.svg');
+
+    default:
+      return SvgPicture.asset('assets/icons/circle_delete.svg');
+  }
+}
