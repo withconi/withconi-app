2781b088c137596ac45a9cb63316ad6a9e0f066f
yellowtoast <kjh9519@naver.com>
2022-10-08 16:04:17 +0900

    edit: edit comment list tile

--- withconi/lib/ui/widgets/listtile/comment_list_tile.dart
+++ withconi/lib/ui/widgets/listtile/comment_list_tile.dart
@@ -1,7 +1,12 @@
 import 'dart:io';
 
+import 'package:withconi/configs/constants/enum.dart';
+import 'package:withconi/controller/auth_controller.dart';
+import 'package:withconi/controller/community/communty_widgets/more_tap_bottom_sheet.dart';
 import 'package:withconi/ui/widgets/button/icon_button.dart';
 
+import '../../../configs/helpers/calculator.dart';
+import '../../../data/model/comment.dart';
 import '../../../import_basic.dart';
 import '../../pages/community/community_post_list_page.dart';
 import '../badge/badge.dart';
@@ -9,51 +14,39 @@ import '../button/icon_text_button.dart';
 import '../button/like_button.dart';
 
 class WcCommentListTile extends StatelessWidget {
-  WcCommentListTile({
-    Key? key,
-    required this.authorName,
-    required this.uploadAt,
-    required this.contents,
-    required this.liked,
-    required this.likesNum,
-    required this.commentsNum,
-    this.badgeText,
-    this.badgeBackgroundColor,
-    this.badgeTextColor,
-    this.userProfile,
-    required this.onLikeTap,
-    this.onCommentTap,
-    this.onPostTap,
-    this.activeBadge = true,
-    this.activeComment = true,
-    this.activeLike = true,
-    this.onMoreTap,
-  }) : super(key: key);
+  WcCommentListTile(
+      {Key? key,
+      required this.liked,
+      this.badgeText,
+      this.userProfile,
+      required this.onLikeTap,
+      this.onCommentTap,
+      this.onPostTap,
+      this.activeComment = true,
+      this.activeLike = true,
+      required this.onMoreTap,
+      required this.comment})
+      : super(key: key);
 
-  String authorName;
-  String uploadAt;
   String? badgeText;
-  Color? badgeBackgroundColor;
-  Color? badgeTextColor;
-  String contents;
   bool liked;
-  int likesNum;
-  int commentsNum;
   void Function(bool) onLikeTap;
   void Function()? onCommentTap;
   void Function()? onPostTap;
-  void Function()? onMoreTap;
+  void Function(Comment, MoreOption?)? onMoreTap;
   File? userProfile;
-  bool activeBadge;
   bool activeComment;
   bool activeLike;
+  Comment comment;
 
   @override
   Widget build(BuildContext context) {
+    String uploadAtStr(DateTime createdAt) =>
+        TimeCalculator().calculateUploadAt(createdAt);
     return GestureDetector(
       onTap: onPostTap,
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        padding: EdgeInsets.fromLTRB(20, 15, 0, 15),
         decoration: BoxDecoration(
             border:
                 Border(bottom: BorderSide(width: 1, color: WcColors.grey80))),
@@ -61,82 +54,86 @@ class WcCommentListTile extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CircleAvatar(
-              radius: 16,
-            ),
-            SizedBox(
-              width: 10,
-            ),
             Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                SizedBox(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            alignment: Alignment.topLeft,
-                            height: 19,
-                            child: Row(
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 16,
+                      backgroundColor: WcColors.grey110,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: SizedBox(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  authorName,
-                                  style: GoogleFonts.notoSans(
-                                      color: WcColors.grey200,
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w600,
-                                      height: 1.5),
+                                Container(
+                                  alignment: Alignment.topLeft,
+                                  height: 19,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        comment.nickname,
+                                        style: GoogleFonts.notoSans(
+                                            color: WcColors.grey200,
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w600,
+                                            height: 1.5),
+                                      ),
+                                      Text(
+                                        ' • ${uploadAtStr(comment.createdAt)}',
+                                        style: GoogleFonts.notoSans(
+                                            color: WcColors.grey140,
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w500,
+                                            height: 1.5),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                                Text(
-                                  ' • $uploadAt',
-                                  style: GoogleFonts.notoSans(
-                                      color: WcColors.grey140,
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w500,
-                                      height: 1.5),
+                                SizedBox(
+                                  width: WcWidth - 40 - 46,
+                                  child: IgnorePointer(
+                                    ignoring: true,
+                                    child: TextField(
+                                      decoration: const InputDecoration(
+                                          border: InputBorder.none,
+                                          isDense: true),
+                                      controller: TextEditingController(
+                                          text: comment.content),
+                                      readOnly: true,
+                                      maxLines: 3,
+                                      minLines: 1,
+                                      style: GoogleFonts.notoSans(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                          height: 1.5),
+                                    ),
+                                  ),
                                 ),
-                                (activeBadge)
-                                    ? WcBadge(
-                                        text: badgeText ?? '',
-                                        backgroundColor: badgeBackgroundColor ??
-                                            Colors.transparent,
-                                        textColor: badgeTextColor ??
-                                            Colors.transparent,
-                                      )
-                                    : SizedBox.shrink()
                               ],
-                            ),
-                          ),
-                          SizedBox(
-                            width: WcWidth - 40 - 46,
-                            child: IgnorePointer(
-                              ignoring: true,
-                              child: TextField(
-                                decoration: const InputDecoration(
-                                    border: InputBorder.none, isDense: true),
-                                controller:
-                                    TextEditingController(text: contents),
-                                readOnly: true,
-                                maxLines: 3,
-                                minLines: 1,
-                                style: GoogleFonts.notoSans(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                    height: 1.5),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(
+                Container(
                   height: 35,
-                  width: WcWidth - 40 - 46,
+                  width: WcWidth - 65,
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -145,9 +142,15 @@ class WcCommentListTile extends StatelessWidget {
                           ? Row(
                               children: [
                                 WcLikeButton(
-                                  likeNum: '28',
                                   isLiked: liked,
                                   valueChanged: onLikeTap,
+                                ),
+                                Text(
+                                  comment.likeNum.toString(),
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 14,
+                                    color: WcColors.grey140,
+                                  ),
                                 ),
                                 SizedBox(
                                   width: 18,
@@ -155,23 +158,21 @@ class WcCommentListTile extends StatelessWidget {
                               ],
                             )
                           : SizedBox.shrink(),
-                      (activeComment)
-                          ? WcIconTextButton(
-                              active: true,
-                              activeIconColor: WcColors.grey100,
-                              iconSrc: 'assets/icons/comment.svg',
-                              inactiveIconColor: WcColors.grey100,
-                              onTap: onCommentTap,
-                              text: '34',
-                            )
-                          : SizedBox.shrink(),
                       WcIconButton(
                           active: true,
-                          onTap: onMoreTap,
+                          onTap: () async {
+                            MoreOption? selectedOption =
+                                await showMoreBottomSheet(
+                              userId: AuthController.to.wcUser.value!.uid,
+                              authorId: comment.authorId,
+                              authorName: comment.nickname,
+                            );
+                            onMoreTap!.call(comment, selectedOption);
+                          },
                           iconHeight: 22,
                           touchWidth: 50,
                           touchHeight: 30,
-                          backgroundColor: Colors.white,
+                          iconMainAxisAlignment: MainAxisAlignment.center,
                           iconSrc: 'assets/icons/dots.svg',
                           activeIconColor: WcColors.grey100,
                           inactiveIconColor: WcColors.grey100)
