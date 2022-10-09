869668a7c51d1768f919e865a76a44c730a2d55e
yellowtoast <kjh9519@naver.com>
2022-10-09 19:54:18 +0900

    edit: edit phone button

--- withconi/lib/ui/pages/map/map_widgets/phone_button.dart
+++ withconi/lib/ui/pages/map/map_widgets/phone_button.dart
@@ -1,14 +1,24 @@
+import 'package:withconi/configs/helpers/url_launcher.dart';
+import 'package:withconi/ui/widgets/snackbar.dart';
+
 import '../../../../import_basic.dart';
 
-class PhoneButton extends StatelessWidget {
-  const PhoneButton({
+class PhoneCallButton extends StatelessWidget {
+  PhoneCallButton({
     Key? key,
+    required this.phoneNumber,
   }) : super(key: key);
-
+  String phoneNumber;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: (phoneNumber.isNotEmpty)
+          ? () {
+              UrlLauncher().launchCall(phoneNumber);
+            }
+          : () {
+              showCustomSnackbar(text: '등록된 번호가 없습니다');
+            },
       child: Image.asset(
         'assets/icons/call.png',
         height: 50,
