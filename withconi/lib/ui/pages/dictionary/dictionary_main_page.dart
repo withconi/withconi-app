9c8b517c6298c46752e4057690eb22154e12ebdb
yellowtoast <kjh9519@naver.com>
2022-10-09 19:54:47 +0900

    feat: implement dictionary main page

--- withconi/lib/ui/pages/dictionary/dictionary_main_page.dart
+++ withconi/lib/ui/pages/dictionary/dictionary_main_page.dart
@@ -1,14 +1,22 @@
+import 'dart:ui';
+
+import 'package:flutter_svg/svg.dart';
+import 'package:withconi/controller/community/community_main_controller.dart';
 import 'package:withconi/import_basic.dart';
-
-import '../../../controller/navigation_controller.dart';
-
-import '../../widgets/navbar/bottom_navbar.dart';
+import 'package:withconi/ui/pages/community/community_setting_page.dart';
+import 'package:withconi/ui/pages/dictionary/dictionary_detail_page.dart';
+import 'package:withconi/ui/theme/text_theme.dart';
+import 'package:withconi/ui/widgets/searchbar/search_bar.dart';
+import '../../widgets/button/icon_button.dart';
 
 class DictionaryMainPage extends StatelessWidget {
   const DictionaryMainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    CommunityMainController _controller = Get.find(tag: 'Initial');
+    // CommunityMainController _controller = Get.put(CommunityMainController());
+
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
@@ -20,17 +28,168 @@ class DictionaryMainPage extends StatelessWidget {
               child: SizedBox(
                 width: WcWidth,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text('질병백과',
+                              style: GoogleFonts.notoSans(
+                                  fontWeight: FontWeight.w600, fontSize: 25)),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SearchBarWidget(
+                            hintText: '질병을 검색해보세요',
+                            textController: null,
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('고양이',
+                              style: TextStyle(
+                                  fontFamily: WcFontFamily.notoSans,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                  color: WcColors.blue100,
+                                  height: 1)),
+                          Text('에게 자주 발생하는 질병',
+                              style: TextStyle(
+                                  fontFamily: WcFontFamily.notoSans,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                  color: WcColors.black,
+                                  height: 1)),
+                        ],
+                      ),
+                    ),
                     const SizedBox(
-                      height: 35,
+                      height: 10,
                     ),
-                    SizedBox(
-                      width: WcWidth - 40,
-                      child: Text('질병백과',
-                          style: GoogleFonts.notoSans(
-                              fontWeight: FontWeight.w600, fontSize: 24)),
+                    Obx(
+                      () => Column(
+                        children: _controller.boardList
+                            .map((board) => InkWell(
+                                  onTap: () {
+                                    Get.to(DictionaryDetailPage());
+                                  },
+                                  child: Container(
+                                      alignment: Alignment.centerLeft,
+                                      height: 40,
+                                      width: WcWidth,
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 30),
+                                      child: Row(
+                                        children: [
+                                          Text(
+                                            (_controller.boardList
+                                                        .indexOf(board) +
+                                                    1)
+                                                .toString(),
+                                            style: GoogleFonts.openSans(
+                                                fontWeight: FontWeight.w600,
+                                                color: WcColors.blue100,
+                                                fontSize: 16),
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            board.title,
+                                            style: GoogleFonts.notoSans(
+                                                fontSize: 16),
+                                          ),
+                                        ],
+                                      )),
+                                ))
+                            .toList(),
+                      ),
                     ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('강아지',
+                              style: TextStyle(
+                                  fontFamily: WcFontFamily.notoSans,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                  color: WcColors.orange100,
+                                  height: 1)),
+                          Text('에게 자주 발생하는 질병',
+                              style: TextStyle(
+                                  fontFamily: WcFontFamily.notoSans,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                  color: WcColors.black,
+                                  height: 1)),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Obx(
+                      () => Column(
+                        children: _controller.boardList
+                            .map((board) => InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    alignment: Alignment.centerLeft,
+                                    height: 40,
+                                    width: WcWidth,
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 30),
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          (_controller.boardList
+                                                      .indexOf(board) +
+                                                  1)
+                                              .toString(),
+                                          style: GoogleFonts.openSans(
+                                              fontWeight: FontWeight.w600,
+                                              color: WcColors.orange100,
+                                              fontSize: 16),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          board.title,
+                                          style: GoogleFonts.notoSans(
+                                              fontSize: 16),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ))
+                            .toList(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    )
                   ],
                 ),
               ),
