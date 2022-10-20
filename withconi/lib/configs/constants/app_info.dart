6c83baffa55b866924cb788c15c6ec5a20bb694c
yellowtoast <kjh9519@naver.com>
2022-10-20 21:27:29 +0900

    add: add app info

---
+++ withconi/lib/configs/constants/app_info.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+class AppInfo {
+  static const version = 'v1.0.0';
+  final String os = Platform.operatingSystem;
+  static const androidAppId = 'com.kakao.talk';
+  static const iosAppId = 'id362057947';
+}
