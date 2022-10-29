dfd9c791c7ca4d4d3a1a9c018ff1315e848a2de2
yellowtoast <kjh9519@naver.com>
2022-10-29 22:26:29 +0900

    feat: implement review entity

---
+++ withconi/lib/ui/ui_model/review_entity.dart
@@ -0,0 +1,41 @@
+import 'package:withconi/configs/constants/enum.dart';
+import 'package:withconi/ui/ui_model/review_rate_entity.dart';
+
+class ReviewGroupEntity {
+  ReviewRate reviewRate = ReviewRate.middle;
+  late ReviewEntity reviewRateEntity;
+  int reviewNum = 0;
+
+  ReviewGroupEntity({
+    required this.reviewRate,
+    required this.reviewNum,
+    required this.reviewRateEntity,
+  });
+
+  ReviewGroupEntity.fromJson(Map<String, dynamic> json, ReviewRate reviewRate) {
+    Map<ReviewItems, int> itemMap = {};
+    reviewRate = reviewRate;
+    reviewNum = json['totalReviews'] ?? 0;
+
+    for (ReviewItems reviewItem in ReviewItems.values) {
+      itemMap[reviewItem] = json[reviewItem.code];
+    }
+
+    switch (reviewRate) {
+      case ReviewRate.high:
+        reviewRateEntity = HighReviewEntity(selectedReviewItems: []);
+        reviewRateEntity.reviewItemsMap = itemMap;
+        break;
+
+      case ReviewRate.middle:
+        reviewRateEntity = MiddleReviewEntity(selectedReviewItems: []);
+        reviewRateEntity.reviewItemsMap = itemMap;
+        break;
+
+      case ReviewRate.low:
+        reviewRateEntity = LowReviewEntity(selectedReviewItems: []);
+        reviewRateEntity.reviewItemsMap = itemMap;
+        break;
+    }
+  }
+}
