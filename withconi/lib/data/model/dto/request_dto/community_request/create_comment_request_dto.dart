b3e56068e42afc92e6f4dae38505c603f9a91a7c
yellowtoast <kjh9519@naver.com>
2022-11-04 22:33:42 +0900

    feat: comment 생성 dto 구현

---
+++ withconi/lib/data/model/dto/request_dto/community_request/create_comment_request_dto.dart
@@ -0,0 +1,45 @@
+import 'package:withconi/core/tools/api_url.dart';
+import 'package:withconi/data/model/dto/api_dto/api_call_dto.dart';
+import 'package:withconi/core/network_handling/network_service.dart';
+import 'dart:html';
+import 'package:withconi/data/model/dto/request_dto/abstract_request/request_dto_abstract.dart';
+import 'package:withconi/module/ui_model/comment_ui_model.dart';
+
+import '../../../../../module/ui_model/ui_model_impl/ui_model_impl.dart';
+import '../abstract_request/request_info_abstract.dart';
+
+class CreateCommentRequestDTO
+    extends RequestDTO<CreateCommentRequestDTO, CommentUIModel>
+    implements RequestInfo {
+  String authorId;
+  String boardId;
+  String postId;
+  String content;
+
+  CreateCommentRequestDTO.fromData(CommentUIModel data)
+      : content = data.content,
+        authorId = data.authorId,
+        boardId = data.boardId,
+        postId = data.postId,
+        super.fromData(data);
+
+  @override
+  FormData? formData;
+
+  @override
+  RequestType requestType = RequestType.POST;
+
+  @override
+  bool requiresToken = true;
+
+  @override
+  String url = HttpUrl.COMMENT_CREATE;
+
+  @override
+  Map<String, dynamic> get dataMap => {
+        "userId": authorId,
+        "boardId": boardId,
+        "postId": postId,
+        "content": content
+      };
+}
