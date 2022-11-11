6191462f479f606fc6a6c522a5e311fcf7e4e18d
yellowtoast <kjh9519@naver.com>
2022-11-11 19:25:04 +0900

    feat: comment list response dto 구현

---
+++ withconi/lib/data/model/dto/response_dto/community_response/comment_list_response_dto.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../abstract_dto/response_dto.dart';
+
+import 'comment_response_dto.dart';
+part 'comment_list_response_dto.freezed.dart';
+part 'comment_list_response_dto.g.dart';
+
+@freezed
+class CommentListResponseDTO
+    with _$CommentListResponseDTO
+    implements ResponseDTO {
+  @JsonSerializable(explicitToJson: true)
+  factory CommentListResponseDTO({
+    required List<CommentResponseDTO> results,
+    required int totalDocuments,
+  }) = _CommentListResponseDTO;
+
+  factory CommentListResponseDTO.fromJson(Map<String, dynamic> json) =>
+      _$CommentListResponseDTOFromJson(json);
+}
