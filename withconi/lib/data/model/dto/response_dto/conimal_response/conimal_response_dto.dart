d73e7edd8ec09b3b64916982bc323634a218f0a8
yellowtoast <kjh9519@naver.com>
2022-11-11 20:35:17 +0900

    feat: conimal response dto 구현 완료

---
+++ withconi/lib/data/model/dto/response_dto/conimal_response/conimal_response_dto.dart
@@ -0,0 +1,28 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:withconi/data/enums/enum.dart';
+
+import '../../../json_converter/datetime_converter.dart';
+import '../abstract_dto/response_dto.dart';
+import '../disease_response/disease_response_dto.dart';
+part 'conimal_response_dto.freezed.dart';
+part 'conimal_response_dto.g.dart';
+
+@freezed
+class ConimalResponseDTO with _$ConimalResponseDTO implements ResponseDTO {
+  @JsonSerializable(explicitToJson: true)
+  factory ConimalResponseDTO({
+    required String conimalId,
+    required String name,
+    required Species species,
+    required Gender gender,
+    @Default(false) bool isNutralized,
+    @Default('breed') String breed,
+    @Default('') String? userId,
+    @DateTimeConverter() required DateTime birthDate,
+    @DateTimeConverter() required DateTime adoptedDate,
+    @Default([]) List<DiseaseResponseDTO> diseases,
+  }) = _ConimalResponseDTO;
+
+  factory ConimalResponseDTO.fromJson(Map<String, dynamic> json) =>
+      _$ConimalResponseDTOFromJson(json);
+}
---
+++ withconi/lib/data/model/dto/response_dto/conimal_response/conimal_response_dto.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'conimal_response_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ConimalResponseDTO _$$_ConimalResponseDTOFromJson(
+        Map<String, dynamic> json) =>
+    _$_ConimalResponseDTO(
+      conimalId: json['conimalId'] as String,
+      name: json['name'] as String,
+      species: $enumDecode(_$SpeciesEnumMap, json['species']),
+      gender: $enumDecode(_$GenderEnumMap, json['gender']),
+      isNutralized: json['isNutralized'] as bool? ?? false,
+      breed: json['breed'] as String? ?? 'breed',
+      userId: json['userId'] as String? ?? '',
+      birthDate: const DateTimeConverter().fromJson(json['birthDate'] as int),
+      adoptedDate:
+          const DateTimeConverter().fromJson(json['adoptedDate'] as int),
+      diseases: (json['diseases'] as List<dynamic>?)
+              ?.map(
+                  (e) => DiseaseResponseDTO.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+    );
+
+Map<String, dynamic> _$$_ConimalResponseDTOToJson(
+        _$_ConimalResponseDTO instance) =>
+    <String, dynamic>{
+      'conimalId': instance.conimalId,
+      'name': instance.name,
+      'species': _$SpeciesEnumMap[instance.species]!,
+      'gender': _$GenderEnumMap[instance.gender]!,
+      'isNutralized': instance.isNutralized,
+      'breed': instance.breed,
+      'userId': instance.userId,
+      'birthDate': const DateTimeConverter().toJson(instance.birthDate),
+      'adoptedDate': const DateTimeConverter().toJson(instance.adoptedDate),
+      'diseases': instance.diseases.map((e) => e.toJson()).toList(),
+    };
+
+const _$SpeciesEnumMap = {
+  Species.all: 'all',
+  Species.cat: 'cat',
+  Species.dog: 'dog',
+};
+
+const _$GenderEnumMap = {
+  Gender.female: 'female',
+  Gender.male: 'male',
+};
