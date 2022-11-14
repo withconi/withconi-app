0269814cd533e12628a37b0955edcd96358389d3
yellowtoast <kjh9519@naver.com>
2022-11-14 18:48:11 +0900

    feat: implement infinite scroll

---
+++ withconi/lib/core/tools/helpers/infinite_scroll.dart
@@ -0,0 +1,43 @@
+import 'package:withconi/import_basic.dart';
+import '../../../module/page_status.dart';
+
+abstract class InfiniteScroll {
+  late final Rx<PaginationFilter> _paginationFilter;
+  late ScrollController infiniteScrollController;
+  double get nextPageTrigger;
+
+  late Rx<InfiniteScrollPageStatus> pageStatus;
+
+  void addInfiniteScrollListener() {}
+
+  void loadNextPage() => changePaginationFilter(1, 3);
+  void loadNewPage() => changePaginationFilter(1, 3);
+
+  void changePaginationFilter(int page, int listSize) {
+    _paginationFilter.value.page = page;
+    _paginationFilter.value.listSize = listSize;
+  }
+
+  Future<void> getDataByPaginationFilter(
+      PaginationFilter _paginationFilter) async {}
+}
+
+class PaginationFilter {
+  int page;
+  int listSize;
+
+  PaginationFilter({required this.page, required this.listSize, e});
+
+  @override
+  String toString() => 'PaginationFilter(page: $page, limit: $listSize)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is PaginationFilter && o.page == page && o.listSize == listSize;
+  }
+
+  @override
+  int get hashCode => page.hashCode ^ listSize.hashCode;
+}
