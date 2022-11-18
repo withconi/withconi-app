beee97161b0392a2e4b07295859a43c0d5b33453
yellowtoast <kjh9519@naver.com>
2022-11-18 21:02:58 +0900

    feat: api call dto 생성

---
+++ withconi/lib/data/model/dto/api_call_dto.dart
@@ -0,0 +1,63 @@
+import 'package:dio/dio.dart';
+import 'package:withconi/data/model/dto/request_dto/abstract_request/request_info_abstract.dart';
+
+import '../../../core/network_handling/network_service.dart';
+import '../../../core/values/constants/app_info.dart';
+
+final Map<String, dynamic> defaultHeader = {
+  'Content-type': 'application/json',
+  'application': AppInfo().os + '_' + AppInfo.version,
+};
+
+class ApiCallDTO {
+  String url;
+  Map<String, dynamic>? header;
+  Map<String, dynamic>? data;
+  FormData? formData;
+  RequestType requestType;
+
+  ApiCallDTO._(
+      {required this.url,
+      required this.data,
+      this.formData,
+      required this.requestType,
+      this.header});
+
+  factory ApiCallDTO.fromDTO(RequestDTO requestDTO) {
+    Map<String, dynamic> header = Map.from(defaultHeader);
+    if (requestDTO.requiresToken) {
+      header['requiresToken'] = requestDTO.requiresToken;
+    }
+
+    if (requestDTO.requestType == RequestType.POST_FORM_DATA) {
+      header.remove('Content-Type');
+      header['Content-Type'] = "multipart/form-data";
+    }
+
+    return ApiCallDTO._(
+        url: requestDTO.url,
+        header: header,
+        data: requestDTO.dataMap,
+        requestType: requestDTO.requestType);
+  }
+
+  factory ApiCallDTO.fromDataDtoWithFormData(
+      RequestDTO requestDTO, FormData formData) {
+    Map<String, dynamic> header = defaultHeader;
+    if (requestDTO.requiresToken) {
+      header['requiresToken'] = requestDTO.requiresToken;
+    }
+
+    if (requestDTO.requestType == RequestType.POST_FORM_DATA) {
+      header.remove('Content-Type');
+      header['Content-Type'] = "multipart/form-data";
+    }
+
+    return ApiCallDTO._(
+        url: requestDTO.url,
+        header: header,
+        data: requestDTO.dataMap,
+        formData: formData,
+        requestType: requestDTO.requestType);
+  }
+}
