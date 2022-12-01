f9c3aa1ffc752e95b3e1d1a01eb7ebc8df90851b
yellowtoast <kjh9519@naver.com>
2022-12-01 20:29:06 +0900

    feat: fcm refresh request dto 구현

---
+++ withconi/lib/data/model/dto/request_dto/fcm_request/fcm_create_request.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_void_to_null
+
+import 'package:dartz/dartz.dart';
+import 'package:dio/src/form_data.dart';
+import 'package:withconi/core/network_handling/network_service.dart';
+import 'package:withconi/core/tools/api_url.dart';
+import 'package:withconi/core/tools/helpers/infinite_scroll.dart';
+import 'package:withconi/data/model/dto/request_dto/abstract_request/request_info_abstract.dart';
+import '../../../../enums/enum.dart';
+import '../abstract_request/request_dto_abstract.dart';
+
+class CreateFcmTokenRequest
+    extends RequestConverter<CreateFcmTokenRequest, String>
+    implements RequestDTO {
+  final String fcmToken;
+  @override
+  bool get requiresToken => true;
+
+  // TODO: GetCustomTokenDTO Post 맞는지 확인하기
+  @override
+  RequestType get requestType => RequestType.POST;
+
+  @override
+  String get url => HttpUrl.FCM_REFRESH;
+
+  CreateFcmTokenRequest.fromData(this.fcmToken) : super.fromData(fcmToken);
+
+  @override
+  Map<String, dynamic> get dataMap => {'newToken': fcmToken};
+}
