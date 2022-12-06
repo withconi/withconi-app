8555fdb5e32c6f8f765b5b0fd2969a6d23f56452
yellowtoast <kjh9519@naver.com>
2022-12-06 18:18:41 +0900

    feat: disease detail request dto 구현함

---
+++ withconi/lib/data/model/dto/request_dto/disease_request/get_disease_detail_request_dto.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:withconi/core/network_handling/network_service.dart';
+import 'package:withconi/core/tools/api_url.dart';
+
+import '../../../../../core/tools/helpers/infinite_scroll.dart';
+import '../abstract_request/request_dto_abstract.dart';
+import '../abstract_request/request_info_abstract.dart';
+
+class GetDiseaseDetailRequestDTO
+    extends RequestConverter<GetDiseaseDetailRequestDTO, String>
+    implements RequestDTO {
+  String diseaseCode;
+
+  @override
+  String get url => HttpUrl.DISEASE_DETAIL_GET;
+
+  @override
+  Map<String, dynamic> get dataMap => {
+        "code": diseaseCode,
+      };
+
+  @override
+  RequestType get requestType => RequestType.GET;
+
+  @override
+  bool get requiresToken => true;
+
+  GetDiseaseDetailRequestDTO.fromData({required this.diseaseCode})
+      : super.fromData(diseaseCode);
+}
