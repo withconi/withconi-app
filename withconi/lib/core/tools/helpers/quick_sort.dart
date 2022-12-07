63beb4eacb57a9d9047194302883630db01856ee
yellowtoast <kjh9519@naver.com>
2022-12-07 21:46:20 +0900

    edit: quick sort수정

--- withconi/lib/core/tools/helpers/quick_sort.dart
+++ withconi/lib/core/tools/helpers/quick_sort.dart
@@ -1,44 +1,102 @@
+import 'package:withconi/module/ui_model/latlng_ui_model.dart';
 import 'package:withconi/module/ui_model/place_marker_ui_model.dart';
 import 'package:withconi/module/ui_model/place_ui_model/abstract_class/place_preview_ui.dart';
 
 import '../../../data/enums/enum.dart';
 
 class QuickSort {
-  sortPlaceByType(
-      {required List<PlacePreviewUiModel> placeMarkerList,
-      required SortType sortType}) {
-    int high = placeMarkerList.length - 1;
+  static final QuickSort _instance = QuickSort._internal();
+
+  QuickSort._internal();
+
+  factory QuickSort() {
+    return _instance;
+  }
+
+  // sortPlaceByType(
+  //     {required List<PlacePreviewUiModel> placeList,
+  //     required SortType sortType}) {
+  //   int high = placeList.length - 1;
+  //   int low = 0;
+  //   List<PlacePreviewUiModel> result =
+  //       _quickSort(placeList, low, high, sortType, baseLocation);
+
+  //   return result;
+  // }
+
+  sortByVisiting({
+    required List<PlacePreviewUiModel> placeList,
+  }) {
+    int high = placeList.length - 1;
     int low = 0;
-    List<PlacePreviewUiModel> result =
-        _quickSort(placeMarkerList, low, high, sortType);
+    List<PlacePreviewUiModel> result = _visitingQuickSort(placeList, low, high);
 
     return result;
   }
 
-  List<PlacePreviewUiModel> _quickSort(
-      List<PlacePreviewUiModel> list, int low, int high, SortType sortType) {
+  sortByNearestWithBaseLocation(
+      {required List<PlacePreviewUiModel> placeList,
+      required LatLngUIModel baseLocation}) {
+    int high = placeList.length - 1;
+    int low = 0;
+
+    List<PlacePreviewUiModel> result =
+        _distanceQuickSort(placeList, low, high, Sorting.nearest, baseLocation);
+
+    return result;
+  }
+
+  // List<PlacePreviewUiModel> _quickSort(List<PlacePreviewUiModel> list, int low,
+  //     int high, SortType sortType, LatLngUIModel baseLocation) {
+  //   if (low < high) {
+  //     late int pi;
+
+  //     if (sortType == SortType.visiting) {
+  //       pi = visitingPartition(list, low, high);
+  //     } else if (sortType == SortType.nearest) {
+  //       pi = distancePartition(list, low, high, baseLocation);
+  //     }
+  //     _quickSort(list, low, pi - 1, sortType, baseLocation);
+  //     _quickSort(list, pi + 1, high, sortType, baseLocation);
+  //   }
+  //   return list;
+  // }
+
+  List<PlacePreviewUiModel> _distanceQuickSort(List<PlacePreviewUiModel> list,
+      int low, int high, Sorting sortType, LatLngUIModel baseLocation) {
     if (low < high) {
       late int pi;
 
-      if (sortType == SortType.visiting) {
-        pi = visitingPartition(list, low, high);
-      } else if (sortType == SortType.nearest) {
-        pi = distancePartition(list, low, high);
-      }
-      _quickSort(list, low, pi - 1, sortType);
-      _quickSort(list, pi + 1, high, sortType);
+      pi = distancePartition(list, low, high, baseLocation);
+
+      _distanceQuickSort(list, low, pi - 1, sortType, baseLocation);
+      _distanceQuickSort(list, pi + 1, high, sortType, baseLocation);
     }
     return list;
   }
 
-  int distancePartition(List<PlacePreviewUiModel> list, low, high) {
+  List<PlacePreviewUiModel> _visitingQuickSort(
+      List<PlacePreviewUiModel> list, int low, int high) {
+    if (low < high) {
+      late int pi;
+
+      pi = visitingPartition(list, low, high);
+
+      _visitingQuickSort(list, low, pi - 1);
+      _visitingQuickSort(list, pi + 1, high);
+    }
+    return list;
+  }
+
+  int distancePartition(
+      List<PlacePreviewUiModel> list, low, high, LatLngUIModel baseLocation) {
     if (list.isEmpty) {
       return 0;
     }
-    double pivot = list[high].meterDistance;
+    double pivot = list[high].meterDistance(baseLocation);
     int i = low - 1;
     for (int j = low; j < high; j++) {
-      if (list[j].meterDistance < pivot) {
+      if (list[j].meterDistance(baseLocation) < pivot) {
         i++;
         swap(list, i, j);
       }
