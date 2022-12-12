7b5e3f20d106e76c3f2c74897c2a3e0feee430a9
yellowtoast <kjh9519@naver.com>
2022-12-12 17:44:01 +0900

    settings: 기본 native setting 파일 수정

--- withconi/ios/Runner/AppDelegate.swift
+++ withconi/ios/Runner/AppDelegate.swift
@@ -8,40 +8,47 @@ import NaverThirdPartyLogin
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        
+
+        if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        }
+
+                
         GeneratedPluginRegistrant.register(with: self)
         
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-    
-    // override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
 
 
-      
-    //     var applicationResult = false
-    //     if (!applicationResult) {
-    //         applicationResult = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
-    //     }
-    //     // if you use other application url process, please add code here.
-        
-    //     if (!applicationResult) {
-    //         applicationResult = super.application(app, open: url, options: options)
-    //     }
-    //     return applicationResult
-    // }
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         var result = false
         
         NSLog("URL = \(url.absoluteString)")
         
+   
+        // if !result {
+        //     result = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+        // }
+        // return result
+
+        // if url.absoluteString.hasPrefix("naver"){
+        //     result = super.application(app, open: url, options: options)
+        // }
+        
+        //네이버 앱 깔려있는 경우 Invalid Date 오류 뜨는 부분 때문에 추가함
+        if (!result) {
+           result = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+        }
         if url.absoluteString.hasPrefix("kakao"){
             result = super.application(app, open: url, options: options)
         }
-        if !result {
-            result = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+
+        if (!result) {
+           result = super.application(app, open: url, options: options)
         }
-        
         return result
+        //네이버 앱 깔려있는 경우 Invalid Date 오류 뜨는 부분 때문에 추가함 
+
     }
     
 }
