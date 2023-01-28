524d6550e281527ab8fc5472f160049c65728cdd
yellowtoast <kjh9519@naver.com>
2023-01-28 21:18:01 +0900

    version: 안드로이드 버전 변경

--- withconi/lib/core/values/constants/app_info.dart
+++ withconi/lib/core/values/constants/app_info.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 class AppInfo {
   static const iosVersion = '1.0.1';
-  static const androidVersion = '1.0.1';
+  static const androidVersion = '1.0.2';
   final String os = Platform.operatingSystem;
   static const androidAppId = 'co.yellowtoast.withconi';
   static const iosAppId = '1661081614';
