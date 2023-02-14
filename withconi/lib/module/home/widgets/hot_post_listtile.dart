c409f9d5f2fb5af20f69a59839877b0987662876
yellowtoast <kjh9519@naver.com>
2023-02-14 20:10:00 +0900

    edit: hot post list tile ui 업데이트 변경

--- withconi/lib/module/home/widgets/hot_post_listtile.dart
+++ withconi/lib/module/home/widgets/hot_post_listtile.dart
@@ -1,5 +1,7 @@
 import 'package:withconi/module/ui_model/post_ui_model.dart';
 
+import '../../../global_widgets/button/icon_text_button.dart';
+import '../../../global_widgets/listtile/post_list_tile.dart';
 import '../../../import_basic.dart';
 import '../../theme/text_theme.dart';
 
@@ -22,39 +24,70 @@ class HotPostListTile extends StatelessWidget {
         onPostTap.call(hotPost);
       },
       child: Container(
-        // color: WcColors.white,
-        alignment: Alignment.center,
-        height: 37,
-        width: width,
-        padding: padding,
-        child: Row(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 13),
+        width: (WcWidth(context) - 20) / 2,
+        decoration: BoxDecoration(
+            color: WcColors.grey20, borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              '${hotPost.diseaseType.displayName} 게시판',
-              style: TextStyle(
-                  fontFamily: WcFontFamily.notoSans,
-                  fontSize: 14.4,
-                  height: 1,
-                  fontWeight: FontWeight.w500,
-                  color: WcColors.black),
+            Row(
+              children: [
+                Image.asset(
+                  hotPost.diseaseType.iconUrl,
+                  height: 13,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(hotPost.diseaseType.displayName + ' 게시판',
+                    style: TextStyle(
+                        fontFamily: WcFontFamily.notoSans,
+                        fontWeight: FontWeight.w500,
+                        height: 1,
+                        color: WcColors.grey180,
+                        fontSize: 12)),
+              ],
             ),
             SizedBox(
-              width: 12,
+              height: 8,
             ),
             Expanded(
               child: Text(
                 hotPost.content,
-                overflow: TextOverflow.fade,
+                overflow: TextOverflow.ellipsis,
                 softWrap: false,
-                maxLines: 1,
+                maxLines: 2,
                 style: TextStyle(
                     fontFamily: WcFontFamily.notoSans,
-                    fontSize: 14.7,
-                    height: 1,
-                    fontWeight: FontWeight.w400,
-                    color: WcColors.grey200),
+                    fontWeight: FontWeight.w500,
+                    height: 1.5,
+                    color: WcColors.grey200,
+                    fontSize: 15),
               ),
             ),
+            Row(
+              children: [
+                WcLikeButton(
+                  isLikeOn: false,
+                  iconSize: 16,
+                  likeNum: hotPost.likeNum,
+                  onLikeTap: (bool) {},
+                  padding: EdgeInsets.only(right: 10),
+                ),
+                WcIconTextButton(
+                  active: false,
+                  activeIconColor: WcColors.grey100,
+                  iconSrc: 'assets/icons/comment.svg',
+                  iconHeight: 15,
+                  inactiveIconColor: WcColors.grey100,
+                  onTap: () {},
+                  text: hotPost.commentNum.toString(),
+                  padding: EdgeInsets.zero,
+                ),
+              ],
+            )
           ],
         ),
       ),
