9c14ef93bacf1bde5836185dc104d62336f44ea1
jihye <kjh9519@naver.com>
2023-02-15 23:43:27 +0900

    edit: bottom navbar 수정

--- withconi/lib/global_widgets/navbar/bottom_navbar.dart
+++ withconi/lib/global_widgets/navbar/bottom_navbar.dart
@@ -1,82 +1,5 @@
-// import 'package:flutter_svg/svg.dart';
-
-// import '../../../controller/auth_controller.dart';
-// import '../../../import_basic.dart';
-
-// class WcBottomNavBar extends StatelessWidget {
-//   WcBottomNavBar({
-//     Key? key,
-//     required this.currentNavIndex,
-//     required this.onTap,
-//   }) : super(key: key);
-//   int currentNavIndex;
-//   void Function(int)? onTap;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BottomNavigationBar(
-//       elevation: 7,
-//       backgroundColor: WcColors.white,
-//       type: BottomNavigationBarType.fixed,
-//       showUnselectedLabels: true,
-//       unselectedItemColor: WcColors.grey120,
-//       unselectedLabelStyle: GoogleFonts.notoSans(
-//         color: WcColors.grey110,
-//         fontSize: 11,
-//         height: 1.7,
-//       ),
-//       selectedLabelStyle: GoogleFonts.notoSans(
-//         color: WcColors.black,
-//         fontSize: 11,
-//         height: 1.7,
-//       ),
-//       items: <BottomNavigationBarItem>[
-//         BottomNavigationBarItem(
-//           icon: SvgPicture.asset(
-//             'assets/icons/nav_home.svg',
-//             color: (currentNavIndex == 0) ? WcColors.black : WcColors.grey100,
-//           ),
-//           label: '홈',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: SvgPicture.asset(
-//             'assets/icons/nav_diagnosis.svg',
-//             color: (currentNavIndex == 1) ? WcColors.black : WcColors.grey100,
-//           ),
-//           label: '자가진단',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: SvgPicture.asset(
-//             'assets/icons/nav_hospital.svg',
-//             color: (currentNavIndex == 2) ? WcColors.black : WcColors.grey100,
-//           ),
-//           label: '병원/약국',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: SvgPicture.asset(
-//             'assets/icons/nav_community.svg',
-//             color: (currentNavIndex == 3) ? WcColors.black : WcColors.grey100,
-//           ),
-//           label: '커뮤니티',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: SvgPicture.asset(
-//             'assets/icons/nav_dictionary.svg',
-//             color: (currentNavIndex == 4) ? WcColors.black : WcColors.grey100,
-//           ),
-//           label: '질병백과',
-//         ),
-//       ],
-//       currentIndex: currentNavIndex, // 지정 인덱스로 이동
-//       selectedItemColor: WcColors.black,
-//       onTap: onTap, // 선언했던 onItemTapped
-//     );
-//   }
-// }
 import 'package:flutter_svg/svg.dart';
 import 'package:withconi/module/theme/text_theme.dart';
-
-import '../../module/auth/auth_controller.dart';
 import '../../import_basic.dart';
 import '../../module/ui_model/nav_destination_ui_model.dart';
 
@@ -99,13 +22,13 @@ class WcBottomNavBar extends StatelessWidget {
       type: BottomNavigationBarType.fixed,
       showUnselectedLabels: true,
       unselectedItemColor: WcColors.grey120,
-      unselectedLabelStyle: TextStyle(
+      unselectedLabelStyle: const TextStyle(
         fontFamily: WcFontFamily.notoSans,
         fontSize: 11.2,
         height: 1.7,
       ),
 
-      selectedLabelStyle: TextStyle(
+      selectedLabelStyle: const TextStyle(
         fontFamily: WcFontFamily.notoSans,
         fontSize: 11.2,
         height: 1.7,
@@ -120,43 +43,7 @@ class WcBottomNavBar extends StatelessWidget {
               ),
               label: e.title))
           .toList(),
-      // items: <BottomNavigationBarItem>[
-      //   BottomNavigationBarItem(
-      //     icon: SvgPicture.asset(
-      //       'assets/icons/nav_home.svg',
-      //       color: (navDestination.title == 0) ? WcColors.black : WcColors.grey100,
-      //     ),
-      //     label: '홈',
-      //   ),
-      //   BottomNavigationBarItem(
-      //     icon: SvgPicture.asset(
-      //       'assets/icons/nav_diagnosis.svg',
-      //       color: (currentNavIndex == 1) ? WcColors.black : WcColors.grey100,
-      //     ),
-      //     label: '자가진단',
-      //   ),
-      //   BottomNavigationBarItem(
-      //     icon: SvgPicture.asset(
-      //       'assets/icons/nav_hospital.svg',
-      //       color: (currentNavIndex == 2) ? WcColors.black : WcColors.grey100,
-      //     ),
-      //     label: '병원/약국',
-      //   ),
-      //   BottomNavigationBarItem(
-      //     icon: SvgPicture.asset(
-      //       'assets/icons/nav_community.svg',
-      //       color: (currentNavIndex == 3) ? WcColors.black : WcColors.grey100,
-      //     ),
-      //     label: '커뮤니티',
-      //   ),
-      //   BottomNavigationBarItem(
-      //     icon: SvgPicture.asset(
-      //       'assets/icons/nav_dictionary.svg',
-      //       color: (currentNavIndex == 4) ? WcColors.black : WcColors.grey100,
-      //     ),
-      //     label: '질병백과',
-      //   ),
-      // ],
+
       currentIndex: currentNavIndex, // 지정 인덱스로 이동
       selectedItemColor: WcColors.grey200,
 
